73e661068cc452a3a26b45ebce325994cc76df77
Prakash Raman <prakash.raman.ka@gmail.com>
2016-12-13 21:23:11 +0530

    test api

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import include, url
+from rest_framework.decorators import renderer_classes, api_view
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
 from api.views import errand
 
+@api_view(['GET'])
+@renderer_classes((JSONRenderer, ))
+def test(request):
+    return Response({"message": "Is this a test?", "data": "Yes it is!"});
+
 urlpatterns = [
-    url(r'make/', errand.make)
+    url(r'test/', test),
+    url(r'errand/make/', errand.make)
 ]
