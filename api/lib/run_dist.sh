c29eb95bc3c5957ed7d2a0dd0a59c502ae8512b5
Prakash Raman <prakash.raman.ka@gmail.com>
2017-01-09 00:38:33 +0530

    removed the comments

--- api/lib/run_dist.sh
+++ api/lib/run_dist.sh
@@ -15,10 +15,10 @@ chmod 0400 api/lib/mAdviser_key_pair.pem
 
 # DO NOT FORGET TO UNCOMMENT THIS!!!!
 echo "Running for descr_stats"
-# ssh -i api/lib/mAdviser_key_pair.pem hadoop@ec2-54-88-153-37.compute-1.amazonaws.com spark-submit --master yarn  --deploy-mode client /home/hadoop/codebase/marlabs-bi/bi/scripts/descr_stats.py --input "hdfs://ip-172-31-3-88.ec2.internal:8020/$1" --result "hdfs://ip-172-31-3-88.ec2.internal:8020$2/result.json" --narratives "hdfs://ip-172-31-3-88.ec2.internal:8020$2/narratives.json"
+ssh -i api/lib/mAdviser_key_pair.pem hadoop@ec2-54-88-153-37.compute-1.amazonaws.com spark-submit --master yarn  --deploy-mode client /home/hadoop/codebase/marlabs-bi/bi/scripts/descr_stats.py --input "hdfs://ip-172-31-3-88.ec2.internal:8020/$1" --result "hdfs://ip-172-31-3-88.ec2.internal:8020$2/result.json" --narratives "hdfs://ip-172-31-3-88.ec2.internal:8020$2/narratives.json"
 
 echo "Running for one_way_anova.py"
-# ssh -i api/lib/mAdviser_key_pair.pem hadoop@ec2-54-88-153-37.compute-1.amazonaws.com spark-submit --master yarn  --deploy-mode client /home/hadoop/codebase/marlabs-bi/bi/scripts/one_way_anova.py --input "hdfs://ip-172-31-3-88.ec2.internal:8020/$1" --result "hdfs://ip-172-31-3-88.ec2.internal:8020$2/dimensions-result.json" --narratives "hdfs://ip-172-31-3-88.ec2.internal:8020$2/dimensions-narratives.json"
+ssh -i api/lib/mAdviser_key_pair.pem hadoop@ec2-54-88-153-37.compute-1.amazonaws.com spark-submit --master yarn  --deploy-mode client /home/hadoop/codebase/marlabs-bi/bi/scripts/one_way_anova.py --input "hdfs://ip-172-31-3-88.ec2.internal:8020/$1" --result "hdfs://ip-172-31-3-88.ec2.internal:8020$2/dimensions-result.json" --narratives "hdfs://ip-172-31-3-88.ec2.internal:8020$2/dimensions-narratives.json"
 
 echo "Running regression py"
 ssh -i api/lib/mAdviser_key_pair.pem hadoop@ec2-54-88-153-37.compute-1.amazonaws.com spark-submit --master yarn  --deploy-mode client /home/hadoop/codebase/marlabs-bi/bi/scripts/regression.py --input "hdfs://ip-172-31-3-88.ec2.internal:8020/$1" --result "hdfs://ip-172-31-3-88.ec2.internal:8020$2/reg-result.json" --narratives "hdfs://ip-172-31-3-88.ec2.internal:8020$2/reg-narratives.json"
