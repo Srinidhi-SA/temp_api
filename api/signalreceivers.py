435ad6f9530c440d86b43a9f26588d9cf33fc8e4
Vivekananda Tadala <t.vivekananda@gmail.com>
2017-08-09 07:42:17 +0530

    refactoring

--- api/signalreceivers.py
+++ api/signalreceivers.py
@@ -1,20 +1,25 @@
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from api.models import Dataset, Insight
+from utils import submit_job
 
 
 @receiver(post_save, sender=Dataset)
 def trigger_metadata_creation_job(sender, instance=None, created=False, **kwargs):
     if created:
         print("Dataset got created, trigger a metadata job")
-        #TODO: Dataset got created, trigger a metadata job
+        # TODO: Dataset got created, trigger a metadata job
+        submit_job(
+            api_url='',  # TODO: Fill with proper url <job>
+            class_name='class_path_metadata'
+        )
 
 
 @receiver(post_save, sender=Insight)
 def trigger_Insight_creation_job(sender, instance=None, created=False, **kwargs):
     if created:
         print("Dataset got created, trigger a Insight job")
-        #TODO: Dataset got created, trigger a Insight job
+        # TODO: Dataset got created, trigger a Insight job
 
-#TODO: Write for filter also
+        # TODO: Write for filter also
