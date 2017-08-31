b66dfbf1c7336cefa8676f95bfd7367b4fd8128d
ankush <ankush.patel@marlabs.com>
2017-08-31 16:07:10 +0530

    aasd

--- static/react/confs/dev/env.js
+++ static/react/confs/dev/env.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:9012"; //dev
+const API = "http://madvisordev.marlabsai.com"; //dev
 // const API = "http://.............";   //prod
 //const API = "http://192.168.33.128:8000"; //vivek
 
