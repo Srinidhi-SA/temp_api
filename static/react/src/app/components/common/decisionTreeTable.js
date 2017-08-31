65a27354754066f84ad81103c7a047d7483e3988
harmanMarlabs <harman.sawhney@marlabs.com>
2017-08-31 03:46:33 -0700

    d tree and var selection chnge

--- static/react/src/app/components/common/decisionTreeTable.js
+++ static/react/src/app/components/common/decisionTreeTable.js
@@ -11,89 +11,52 @@ export class DecisionTreeTable extends React.Component {
     super();
   }
   componentDidMount() {
-      HeatMap("heat-table-map");
-      //table.find('tr').each(function (i) {
-      $(function(){
-        $(".idDecisionTreeTable").each(function (i) {
-            var $tds = $(this).find('td');
-            var $divs  =  $tds.eq(1).find('div');
-            var $div2  =  $tds.eq(2).find('div');
-            for(var j=0;j<$divs.length;j++){
-              $($div2[j]).height($($divs[j]).height());
-            }
-            $($div2[$divs.length-1]).css({"border-bottom":"0px"});
-            $($divs[$divs.length-1]).css({"border-bottom":"0px"});
-          });
-      });
-
+    
   }
 
   render() {
    var element = this.props.tableData.tableData;
-   console.log("checking html element");
-   console.log(element);
-   console.log();
-   let thStyle = {
-     "border-bottom": "0px",
-   };
-   let td2Style ={
-         "minHeight":"20px",
-         "padding":"8px",
-         "overflowY":"auto",
-         "borderBottom":"1px solid #e6e6e6",
-         "width":"100%"
-   };
-   let td3Style = {
-       "minHeight": "20px",
-       "padding": "8px",
-       "overflowY": "auto",
-       "borderBottom": "1px solid rgb(230, 230, 230)",
-       "width": "100%",
-       "height": "41px"
-   };
+ 
   let renderTableThead = element[0].map((item,i)=>{
-    if(i==0){
-      return(
-          <th style={thStyle}>{item}</th>
-      );
-    }else if(i==1){
-        return(
-        <th width="70%">{item}</th>
-      );
-    }else if(i==2){
-        return(
-        <th width="10%">{item}</th>
-      );
-    }else{
-      <th>{item}</th>
-    }
+	return(
+        <th>{item}</th>
+	  );
 
 });
 
-     let renderTableTbody = element.map((item,i)=>{
-       if(i!=0){
-           let secondTd =item[1].map((secondItem,secondI)=>{
-              let id="id_rule_"+ secondI;
-                return(
-                  <div key={secondI} style={td2Style} id={id}>{secondItem}</div>
-                  );
-           });
-           let thirdTd =item[2].map((thirdItem,thirdI)=>{
-             let id="id_prop_"+ thirdI;
-                return(
-                  <div style={td3Style} id={id}>{thirdItem}</div>
-                  );
-           });
-              return(
-                <tr key={i}>
-                <td >{item[0]}</td>
-                <td style={{padding:"0px",display:"block"}}>{secondTd}</td>
-                <td style={{padding:"0px"}}>{thirdTd}</td>
-                </tr>
-              );
-        }
+   
+   var trs=[];
+   for (var i=1;i<element.length;i++){
+	   let trElement = element[i], tdElement = element[i][1], len=element[i][1].length;
+	  // alert(len);
+	  //  var renderTableTbody = trElement.map(function(item,index){
+			trs[i] = tdElement.map(function(subItem,index){
+				 if(index==0){
+					 return(
+					    <tr>
+						   <td rowSpan={len}>{element[i][0]}</td>
+						   <td>{element[i][1][index]}</td>
+						   <td>{element[i][2][index]}</td>
+						</tr>
+					 );
+					 
+				 }else{
+					 return(
+					    <tr>
+						   <td>{element[i][1][index]}</td>
+						   <td>{element[i][2][index]}</td>
+						</tr>
+					 );
+					 
+				 }
+			});
+			
+		//});
+	   
+   }// end of for loop
+   
 
-        });
+   
 
 
 
@@ -101,7 +64,7 @@ export class DecisionTreeTable extends React.Component {
 
         <table className="table table-bordered idDecisionTreeTable">
                <thead><tr> {renderTableThead}</tr></thead>
-               <tbody>{renderTableTbody}</tbody>
+               <tbody>{trs}</tbody>
          </table>
 
     );
