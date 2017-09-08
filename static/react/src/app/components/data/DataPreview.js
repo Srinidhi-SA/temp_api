d6c65bfeed6f562fe46e3076c71da4df31510fb0
harmanMarlabs <harman.sawhney@marlabs.com>
2017-09-08 03:33:58 -0700

    bug fixes

--- static/react/src/app/components/data/DataPreview.js
+++ static/react/src/app/components/data/DataPreview.js
@@ -225,7 +225,7 @@ export class DataPreview extends React.Component {
  
 							<div key={i} className="col-md-2 co-sm-4 col-xs-6">
  
-							<h3>
+							<h3 className="text-center">
 							{item.value} <br/><small>{item.displayName}</small>
 							</h3>
 							</div>
