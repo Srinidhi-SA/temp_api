9412d23a12547967c05114e99b1fa8c63810bf84
Swati Gupta <swati.gupta@marlabs.com>
2017-09-13 18:36:19 +0530

    added profile info

--- static/react/src/app/actions/loginActions.js
+++ static/react/src/app/actions/loginActions.js
@@ -45,3 +45,45 @@ function fetchPostsError(json) {
     json
   }
 }
+
+//to fetch user Profile
+
+export function getUserProfile(token) {
+    return (dispatch) => {
+    return fetchUserProfile(token).then(([response, json]) =>{
+        if(response.status === 200){
+        dispatch(fetchProfileSuccess(json))
+      }
+      else{
+        dispatch(fetchProfileError(json))
+      }
+    })
+  }
+}
+
+function fetchUserProfile(token) {
+  // console.log("user and pass is:");
+  // console.log(username+" "+password);
+  return fetch(API+'/api/get_info/',{
+		method: 'GET',
+		headers: {
+      'Authorization': token,
+      'Content-Type': 'application/json'
+		}
+	}).then( response => Promise.all([response, response.json()]));
+}
+
+
+function fetchProfileSuccess(profileInfo) {
+  return {
+    type: "PROFILE_INFO",
+    profileInfo
+  }
+}
+
+function fetchProfileError(json) {
+  return {
+    type: "PROFILE_ERROR",
+    json
+  }
+}
