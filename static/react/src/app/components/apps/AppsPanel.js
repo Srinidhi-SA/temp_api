1f47c9b38015b2b8fd7850f60895c233c82d45a2
PravallikaGunjapalle <gunjapalle.pravallika@marlabs.com>
2017-09-18 06:27:07 +0530

    Added Audio Apps panel

--- static/react/src/app/components/apps/AppsPanel.js
+++ static/react/src/app/components/apps/AppsPanel.js
@@ -140,6 +140,44 @@ export class AppsPanel extends React.Component {
 
 					</div>
 				</div>
+				<div class="clearfix xs-p-20"></div>
+				<div class="row">
+				<div class="col-md-4">
+
+					
+					<div className="app-block"> 
+						<Link onClick={this.gotoAppsList.bind(this,APPID1,APPNAME1)} className="app-link" to='/apps/1/models'>
+
+						<div className="col-md-4 col-sm-3 col-xs-5 xs-p-20">
+							<img src={STATIC_URL + "assets/images/icon_oppr.png"} className="img-responsive"/>
+						</div>
+						<div className="col-md-8 col-sm-9 col-xs-7">
+							<h4>OPPORTUNITY SCORING</h4>
+							<p>
+								Business Goals by understading the top-level business goals, we can infer the investment climate.
+							</p>
+						</div>
+						<div class="clearfix"></div>
+						</Link>
+
+						<div className="card-footer">
+						<ul className="app_labels">
+							<li className="xs-p-10 text-primary"><i className="fa fa-tag fa-1x"></i></li>
+							<li><a href="#"><i className="fa fa-tag"></i> Marketing</a></li>
+							<li><a href="#"><i className="fa fa-tag"></i> Sales</a></li>
+						</ul>
+
+					{/*<div className="card-deatils">
+							<a href="javascript:void(0);" rel="popover" className="pover" data-popover-content="#myPopover" data-original-title="" title=""><i className="ci pe-7s-info pe-2x"></i></a>
+					</div>*/}
+					<div id="myPopover" className="pop_box hide">
+					<p>Info</p>
+					</div>
+					</div>
+					</div>
+
+				</div>
+				</div>
 
 
 		   </div>
