c8153da0f034a943380290dbdceb576ef2a70a76
ankush <ankush.patel@marlabs.com>
2017-09-19 20:35:33 +0530

    chart for speech analysis

--- api/lib/speech_analysis.py
+++ api/lib/speech_analysis.py
@@ -104,6 +104,11 @@ class SpeechAnalyzer:
                 keywords = self.nl_understanding.get("keywords")
                 keywods_html = " ".join( [ "<span>{}</span>".format(item.get("text")) for item in keywords])
                 keywords_card = self.__generate_normal_card("Keywords",keywods_html)
+
+
+                temp_card_data = keywords_card.get("cardData")
+                temp_card_data.append(self.get_keywords_bar(keywords))
+                keywords_card["cardData"] = temp_card_data
             except:
                 pass
             
@@ -113,8 +118,31 @@ class SpeechAnalyzer:
             try:
                 categories_html = "".join([ "<span>{}</span> <span>{}</span>".format(item.get("label").split("/")[-1], item.get("score")) for item in self.nl_understanding.get("categories")])
                 categories_card = self.__generate_normal_card("Categories", categories_html)
+
+                categories = self.nl_understanding.get("categories")
+                temp_card_data = categories_card.get("cardData")
+                temp_card_data.append(self.get_categories_bar(categories))
+                categories_card["cardData"] = temp_card_data
             except:
                 pass
+        #
+        # entities_card = ""
+        # if "entities" in self.nl_understanding:
+        #     try:
+        #         categories_html = "".join(
+        #             ["<span>{}</span> <span>{}</span>".format(item.get("label").split("/")[-1], item.get("score")) for
+        #              item in self.nl_understanding.get("categories")])
+        #         categories_card = self.__generate_normal_card("Categories", categories_html)
+        #
+        #         categories = self.nl_understanding.get("categories")
+        #         temp_card_data = categories_card.get("cardData")
+        #         temp_card_data.append(self.get_categories_bar(categories))
+        #         categories_card["cardData"] = temp_card_data
+        #     except:
+        #         pass
+
+        simple_html_card = self.__generate_normal_card("", self.generate_para_html())
+
 
         if emotions_card:
             list_of_cards.append(emotions_card)
@@ -124,7 +152,10 @@ class SpeechAnalyzer:
             list_of_cards.append(sentiment_card)
         if categories_card:
             list_of_cards.append(categories_card)
-        
+
+        if simple_html_card:
+            list_of_cards.append(simple_html_card)
+
         self.nl_understanding_nodestructure = {
                 "name": "Overview card",
                 "slug": "fdfdfd_overview",
@@ -135,9 +166,17 @@ class SpeechAnalyzer:
         return self.nl_understanding_nodestructure
 
 
-    def __get_emotions_html(self, emo, val):
-        return '<div class="emotion-{}"><span class="emotion-title">{}</span><span class="emotion-percent">{}%</span></div>'.format(emo,emo,val*100)
+    # def __get_emotions_html(self, emo, val):
+    #     return '<div class="emotion-{}"><span class="emotion-title">{}</span><span class="emotion-percent">{}%</span></div>'.format(emo,emo,val*100)
 
+    def __get_emotions_html(self, emo, val):
+
+        return """
+         <div class="col-md-4">
+                 <img src="/static/assessts/images/{}.png" width="80" class="pull-left" >
+                 <h2 class="pull-left"><small>{}</small><br> {}% </h2>
+         </div>
+         """.format(emo, emo, int(val * 100))
 
     def __generate_normal_card(self, name, html):
         return {
@@ -203,6 +242,33 @@ class SpeechAnalyzer:
 
         return gauge_c3_chart_data
 
+    def generate_para_html(self):
+        emotions_sorted = sorted(self.nl_understanding.get("emotion").get("document").get("emotion").items(),
+                                 key=lambda (key, val): val, reverse=True)
+        (best_emotion, best_value) = emotions_sorted[0]
+        (second_best_emotion, second_best_value) = emotions_sorted[1]
+        keywords = self.nl_understanding.get("keywords")
+        keywords_html = " and ".join(
+            ["{} ({})".format(item.get("text"), round(item.get("relevance"), 2)) for item in keywords[:2]])
+
+        categories_html = " and ".join(["<strong>{}</strong>".format(item.get("label").split("/")[-1]) for item in
+                                    self.nl_understanding.get("categories")[:2]])
+
+        return """The overall sentiment in the speech seems to be {} {}.
+            The most predominant emotion is <strong>{}</strong> at around {}%.
+            Another important emotion identified is  <strong>{}</strong> at {}%.
+           mAdvisor identified {} keywords in the speech,
+            {}
+            having the highest relevance.
+           The major categories are {}.
+             """.format(self.nl_understanding.get("sentiment").get("document").get("label"),
+                        round(self.nl_understanding.get("sentiment").get("document").get("score"), 2),
+                        best_emotion, int(best_value * 100),
+                        second_best_emotion, int(second_best_value * 100),
+                        len(keywords),
+                        keywords_html,
+                        categories_html
+                        )
 
     def genarate_slug(self, name):
         from django.template.defaultfilters import slugify
@@ -210,6 +276,113 @@ class SpeechAnalyzer:
         import string
         return slugify(str(name) + "-" + ''.join(
             random.choice(string.ascii_uppercase + string.digits) for _ in range(10)))
+
+
+    def get_bar_chart(self, data, rotate=False, x="label", y="score"):
+
+        c3 = C3chart_ML(
+            axes={
+                "x": x,
+                "y": y
+            },
+            label_text={
+                "x": x,
+                "y": y
+            },
+            data=data,
+            axisRotation=rotate
+        )
+        details = c3.get_details()
+
+        return decode_and_convert_chart_raw_data(details)
+
+    def get_categories_bar(self, categories):
+
+        data = categories
+        return self.get_bar_chart(
+            data=data
+        )
+
+    def get_keywords_bar(self, keywords):
+
+        data = []
+
+        for d in keywords:
+            temp = {}
+            temp['text'] = d.get('text')
+            temp['score'] = d.get('sentiment').get('score')
+            data.append(temp)
+
+        return self.get_bar_chart(
+            data=data,
+            x='text',
+            y='score'
+        )
+
+    def get_entities_bar(self, entities):
+
+        data = []
+
+        for d in entities:
+            temp = {}
+            temp['text'] = d.get('text')
+            temp['relevance'] = d.get('relevance')
+            data.append(temp)
+
+        return self.get_bar_chart(
+            data=data,
+            x='text',
+            y='relevance'
+        )
+
+
+from api.helper import decode_and_convert_chart_raw_data
+
+
+class C3chart_ML(object):
+
+    def __init__(self, **kwrgs):
+        self.chart_type = kwrgs.get('chart_type', 'bar')
+        self.axes = kwrgs.get('axes', {})
+        self.label_text = kwrgs.get('label_text', {})
+        self.types = kwrgs.get('types')
+        self.axisRotation = kwrgs.get('axisRotation', False)
+        self.yAxisNumberFormat = kwrgs.get('yAxisNumberFormat', ".2f")
+        # self.y2AxisNumberFormat = kwrgs.get('y2AxisNumberFormat', False)
+        self.showLegend = kwrgs.get('showLegend', False)
+        self.hide_xtick = kwrgs.get('hide_xtick', False)
+        self.subchart = kwrgs.get('subchart', False)
+        self.rotate = kwrgs.get('rotate', False)
+        self.data = kwrgs.get('data')
+        self.legend = {}
+
+    def get_details(self):
+
+        return {
+            'chart_type': self.chart_type,
+            'axes': self.axes,
+            'label_text': self.label_text,
+            'types': self.types,
+            'axisRotation': self.axisRotation,
+            'yAxisNumberFormat': self.yAxisNumberFormat,
+            'showLegend': self.showLegend,
+            'hide_xtick': self.hide_xtick,
+            'subchart': self.subchart,
+            'rotate': self.rotate,
+            'data': self.data,
+            'legend': self.legend
+        }
+
+    def update_data(self, data):
+        self.data = data
+
+    def rotate_axis(self):
+        self.axisRotation = True
+
+    def update_axes(self, axes):
+        self.axes = axes
+
+
 
 
 
