53040fb7cb3e97a0d520970c352027ff3845542f
ankush <ankush.patel@marlabs.com>
2017-09-19 21:02:45 +0530

    fix

--- api/lib/speech_analysis.py
+++ api/lib/speech_analysis.py
@@ -295,11 +295,16 @@ class SpeechAnalyzer:
         details = c3.get_details()
 
         decoded_chart =  decode_and_convert_chart_raw_data(details)
-        
+
         del decoded_chart['chart_c3']['axis']['x']['tick']['format']
         del decoded_chart['chart_c3']['axis']['y']['tick']['format']
         del decoded_chart['yformat']
-        return decoded_chart
+
+        return {
+            "dataType": "c3Chart",
+            "data": decoded_chart
+        }
+
 
     def get_categories_bar(self, categories):
 
