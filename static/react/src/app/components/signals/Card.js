59ef4cdb885747b99292dbaf71878d37d28e16af
PravallikaGunjapalle <gunjapalle.pravallika@marlabs.com>
2017-10-30 14:22:07 +0530

    formatted card js file

--- static/react/src/app/components/signals/Card.js
+++ static/react/src/app/components/signals/Card.js
@@ -19,8 +19,8 @@ import $ from "jquery";
 
 
 var data = null,
-  yformat = null,
-  cardData = {};
+yformat = null,
+cardData = {};
 /*let d3data = {
     name: 'Parent',
     children: [{
@@ -31,91 +31,91 @@ var data = null,
 };*/
 
 const myTreeData = [
-  {
-    name: 'Top Level',
+                    {
+                    	name: 'Top Level',
 
-    children: [
-      {
-        name: 'Level 2: A',
-		children: [
-			  {
-				name: 'Level 2: A',
+                    	children: [
+                    	           {
+                    	        	   name: 'Level 2: A',
+                    	        	   children: [
+                    	        	              {
+                    	        	            	  name: 'Level 2: A',
 
-			  },
-			  {
-				name: 'Level 2: B',
-			  },
-			],
+                    	        	              },
+                    	        	              {
+                    	        	            	  name: 'Level 2: B',
+                    	        	              },
+                    	        	              ],
 
-      },
-      {
-        name: 'Level 2: B',
-      },
-    ],
-  },
-];
+                    	           },
+                    	           {
+                    	        	   name: 'Level 2: B',
+                    	           },
+                    	           ],
+                    },
+                    ];
 
 @connect((store) => {
-  return {login_response: store.login.login_response, signal: store.signals.signalAnalysis,
-  chartObject: store.chartObject.chartObj};
+	return {login_response: store.login.login_response, signal: store.signals.signalAnalysis,
+		chartObject: store.chartObject.chartObj};
 })
 
 export class Card extends React.Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-   }
-//
-  render() {
-    console.log("card is called!!!! with data:----");
-    cardData = this.props.cardData;
-    //console.log(cardData);
+	}
 
-    const cardElements = cardData.map((story, i) => {
-    	let randomNum = Math.random().toString(36).substr(2,8);
-      switch (story.dataType) {
-        case "html":
+	render() {
+		console.log("card is called!!!! with data:----");
+		cardData = this.props.cardData;
+		//console.log(cardData);
 
-          return (<CardHtml key = {i} htmlElement={story.data} type={story.dataType}/>);
-          break;
-        case "c3Chart":
-        //console.log("checking chart data:::::");
-          if(!$.isEmptyObject(story.data)){
-            if(story.widthPercent){
-              let width  = story.widthPercent+"%"
-           return (<div key={randomNum} style={{width:width, display:"inline-block",paddingLeft:"30px"}}><C3Chart classId={randomNum} widthPercent = {story.widthPercent} data={story.data.chart_c3} yformat={story.data.yformat} y2format={story.data.y2format} guage={story.data.gauge_format} tooltip={story.data.tooltip_c3} tabledata={story.data.table_c3} tabledownload={story.data.download_url} xdata={story.data.xdata}/><div className="clearfix"/></div>);
-           }else{
-             return (<div key={randomNum}><C3Chart classId={randomNum} data={story.data.chart_c3} yformat={story.data.yformat} y2format={story.data.y2format} guage={story.data.gauge_format} tooltip={story.data.tooltip_c3} tabledata={story.data.table_c3} tabledownload={story.data.download_url} xdata={story.data.xdata}/><div className="clearfix"/></div>);
-           }
-         }
-          break;
-          case "tree":
-		  	//console.log("checking tree data");
-            return ( <DecisionTree key={i} treeData={story.data}/>);
-          break;
-        case "table":
-        return (<div className="table-style"><CardTable key = {i} jsonData={story.data} type={story.dataType}/></div>);
-            break;
-		case "dropdown":
-            return (<PredictionDropDown key = {i} jsonData={story.data} type={story.dataType}/>);
-            break;
-		 case "gauge":
-		 return (<GaugeMeter key = {i} jsonData={story.data} type={story.dataType}/>);
-		 break;
-		 case "dataBox":
-			 return (<DataBox key = {i} jsonData={story.data} type={story.dataType}/>);
-			 break;
-			 case "wordCloud":
-				 return (<WordCloud key = {i} jsonData={story.data} type={story.dataType}/>);
-				 break;
-      }
+		const cardElements = cardData.map((story, i) => {
+			let randomNum = Math.random().toString(36).substr(2,8);
+			switch (story.dataType) {
+			case "html":
 
-    });
-    return (
-      <div>
-          {cardElements}
-      </div>
-    );
+				return (<CardHtml key = {i} htmlElement={story.data} type={story.dataType}/>);
+				break;
+			case "c3Chart":
+				//console.log("checking chart data:::::");
+				if(!$.isEmptyObject(story.data)){
+					if(story.widthPercent){
+						let width  = story.widthPercent+"%"
+						return (<div key={randomNum} style={{width:width, display:"inline-block",paddingLeft:"30px"}}><C3Chart classId={randomNum} widthPercent = {story.widthPercent} data={story.data.chart_c3} yformat={story.data.yformat} y2format={story.data.y2format} guage={story.data.gauge_format} tooltip={story.data.tooltip_c3} tabledata={story.data.table_c3} tabledownload={story.data.download_url} xdata={story.data.xdata}/><div className="clearfix"/></div>);
+					}else{
+						return (<div key={randomNum}><C3Chart classId={randomNum} data={story.data.chart_c3} yformat={story.data.yformat} y2format={story.data.y2format} guage={story.data.gauge_format} tooltip={story.data.tooltip_c3} tabledata={story.data.table_c3} tabledownload={story.data.download_url} xdata={story.data.xdata}/><div className="clearfix"/></div>);
+					}
+				}
+				break;
+			case "tree":
+				//console.log("checking tree data");
+				return ( <DecisionTree key={i} treeData={story.data}/>);
+				break;
+			case "table":
+				return (<div className="table-style"><CardTable key = {i} jsonData={story.data} type={story.dataType}/></div>);
+				break;
+			case "dropdown":
+				return (<PredictionDropDown key = {i} jsonData={story.data} type={story.dataType}/>);
+				break;
+			case "gauge":
+				return (<GaugeMeter key = {i} jsonData={story.data} type={story.dataType}/>);
+				break;
+			case "dataBox":
+				return (<DataBox key = {i} jsonData={story.data} type={story.dataType}/>);
+				break;
+			case "wordCloud":
+				return (<WordCloud key = {i} jsonData={story.data} type={story.dataType}/>);
+				break;
+			}
 
-  }
+		});
+		return (
+				<div>
+				{cardElements}
+				</div>
+		);
+
+	}
 }
