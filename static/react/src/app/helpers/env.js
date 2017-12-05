c225728989a3873d88adcd2d138af86a40edcf8a
Unknown <gunjapalle.pravallika@marlabs.com>
2017-12-05 13:48:42 +0530

    Score Preview is fixed

--- static/react/src/app/helpers/env.js
+++ static/react/src/app/helpers/env.js
@@ -1,7 +1,7 @@
 const API = APP_BASE_URL;
 // const API = "http://.............";   //prod
 //const API = "http://192.168.33.128:8000"; //vivek
-const EMR = "http://174.129.163.0:8001";
+const EMR = SCORES_BASE_URL;
 //const API = ""; //vivek
 const STATIC_URL = "/";
 
