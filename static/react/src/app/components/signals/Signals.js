9fa5b6a5322100cd39804fce78141b0eb6f15a49
Unknown <gunjapalle.pravallika@marlabs.com>
2017-12-05 11:58:43 +0530

    Signal conflict issue is resolved

--- static/react/src/app/components/signals/Signals.js
+++ static/react/src/app/components/signals/Signals.js
@@ -178,7 +178,7 @@ export class Signals extends React.Component {
         var signalLink = "/signals/" + story.slug;
         return (
 
-          <div className="col-md-3 top20 list-boxes" key={i}>
+          <div className="col-md-3 xs-mb-10 xs-pr-5 xs-pl-5 list-boxes" key={i}>
             <div className="rep_block newCardStyle" name={story.name}>
               <div className="card-header"></div>
               <div className="card-center-tile">
@@ -198,7 +198,7 @@ export class Signals extends React.Component {
               <div className="card-footer">
                 <div className="left_div">
                   <span className="footerTitle"></span>{story.username}
-                  <span className="footerTitle">{dateFormat(story.created_at, "mmm d,yyyy HH:MM")}</span>
+                  <span className="footerTitle footerTitle-lineh">{dateFormat(story.created_at, "mmm d,yyyy HH:MM")}</span>
                 </div>
 
                 <div className="card-deatils">
@@ -247,20 +247,20 @@ export class Signals extends React.Component {
                 <div class="col-md-8">
                   <h3 className="xs-mt-0">Signals</h3>
                 </div>
-                <div class="col-md-4">				
-				<div class="btn-toolbar pull-right">
-				<div class="input-group">
-				<div className="search-wrapper">
-					<form>
-					<input type="text" name="search_signals" onKeyPress={this._handleKeyPress.bind(this)} onChange={this.onChangeOfSearchBox.bind(this)} title="Search Signals" id="search_signals" className="form-control search-box" placeholder="Search signals..." required />
-					<span class="fa fa-search form-control-feedback"></span>
-					<button className="close-icon" type="reset"></button>
-					</form>
-				</div>
-				</div>
+                <div class="col-md-4">              
+                <div class="btn-toolbar pull-right">
+                <div class="input-group">
+                <div className="search-wrapper">
+                    <form>
+                    <input type="text" name="search_signals" onKeyPress={this._handleKeyPress.bind(this)} onChange={this.onChangeOfSearchBox.bind(this)} title="Search Signals" id="search_signals" className="form-control search-box" placeholder="Search signals..." required />
+                    <span class="fa fa-search form-control-feedback"></span>
+                    <button className="close-icon" type="reset"></button>
+                    </form>
+                </div>
+                </div>
                   <div class="btn-group">
                     <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><i class="fa fa-sort-alpha-asc fa-lg"></i> <span class="caret"></span></button>
-					<ul role="menu" class="dropdown-menu dropdown-menu-right">
+                    <ul role="menu" class="dropdown-menu dropdown-menu-right">
                         <li>
                           <a href="#" onClick={this.doSorting.bind(this,'name','asc')}><i class="fa fa-sort-alpha-asc" aria-hidden="true"></i> Name Ascending</a>
                         </li>
@@ -280,15 +280,15 @@ export class Signals extends React.Component {
               </div>
             </div>
 
-            <div class="clearfix"></div>
-          </div>
+            
 
           <div className="main-content">
-            <div className="row">
+            <div className="row xs-m-0">
               {addButton}
               {storyList}
               <div className="clearfix"></div>
             </div>
+            
             <div className="ma-datatable-footer" id="idSignalPagination">
               <div className="dataTables_paginate">
                 {paginationTag}
