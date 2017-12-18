7b955776949bc24eaf4ce55a592533bbe6978226
Swati Gupta <swati.gupta@marlabs.com>
2017-12-18 15:18:04 +0530

    profile page load

--- static/react/src/app/components/common/profile.js
+++ static/react/src/app/components/common/profile.js
@@ -33,9 +33,9 @@ export class Profile extends React.Component {
     console.log(props)
   }
   componentWillMount() {
-    if (isEmpty(this.props.profileInfo))
+  //  if (isEmpty(this.props.profileInfo))
       this.props.dispatch(getUserProfile(getUserDetailsOrRestart.get().userToken))
-    if (this.props.profileImgURL == "")
+  // if (this.props.profileImgURL == "")
       this.props.dispatch(saveProfileImage(getUserDetailsOrRestart.get().image_url))
 
   }
