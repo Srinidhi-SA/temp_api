4125d94b4ae0cc553635a729f0de6da483534ae1
Swati Gupta <swati.gupta@marlabs.com>
2017-12-20 17:43:02 +0530

    added fix to chart options

--- config/settings/base.py
+++ config/settings/base.py
@@ -527,7 +527,7 @@ YARN_STATUS = { "RUNNING" :"INPROGRESS",
                 "SUCCESS" : "SUCCESS"
               }
 
-UI_VERSION = "v2.1_37"
+UI_VERSION = "v2.1_40"
 
 ERROR_MESSAGE = {
     'upload_error': 'Code had a weakness. Now it is broken.'
