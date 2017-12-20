c069c2260ded0ba3d3c0c361622f0d5ff2324769
Swati Gupta <swati.gupta@marlabs.com>
2017-12-20 16:46:03 +0530

    changed version

--- config/settings/base.py
+++ config/settings/base.py
@@ -527,7 +527,7 @@ YARN_STATUS = { "RUNNING" :"INPROGRESS",
                 "SUCCESS" : "SUCCESS"
               }
 
-UI_VERSION = "v2.1_37"
+UI_VERSION = "v2.1_38"
 
 ERROR_MESSAGE = {
     'upload_error': 'Code had a weakness. Now it is broken.'
