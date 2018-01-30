7d7d6e86dd94600d97a6dfe2d3b50309ec2409f5
ankush <ankush.patel@marlabs.com>
2018-01-30 19:05:54 +0530

    score fix

--- api/views.py
+++ api/views.py
@@ -280,7 +280,6 @@ class ScoreView(viewsets.ModelViewSet):
         data['trainer'] = Trainer.objects.filter(slug=data['trainer'])
         data['dataset'] = Dataset.objects.filter(slug=data['dataset'])
         data['created_by'] = request.user.id  # "Incorrect type. Expected pk value, received User."
-        import pdb;pdb.set_trace()
         data['app_id'] = int(json.loads(data['config'])['app_id'])
         serializer = ScoreSerlializer(data=data)
         if serializer.is_valid():
