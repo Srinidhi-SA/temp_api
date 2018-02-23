f38d991cab8993fbf2b2767442a9aac994508bb4
ankush <ankush.patel@marlabs.com>
2018-02-23 17:20:40 +0530

    removed os kill

--- api/tasks.py
+++ api/tasks.py
@@ -48,7 +48,6 @@ def submit_job_separate_task(command_array, slug):
             model_instance.url = application_id
             model_instance.save()
             break
-    os.killpg(os.getpgid(cur_process.pid), signal.SIGTERM)
 
 @task(name='write_into_databases')
 def write_into_databases(job_type, object_slug, results):
