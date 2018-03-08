5f068dade6b88f1c05c0ea1deb20af128fd14690
ankush <ankush.patel@marlabs.com>
2018-03-08 15:34:05 +0530

    production change

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -526,8 +526,8 @@ def configuration_details():
                 "react_path": "/static/react",
                 "asset_path": "/static/asset",
                 "base_remote_path": "/home/ubuntu/codebase/mAdvisor-api",
-                "ui_branch": "api_ui_dev_staging",
-                "api_branch": "api_ui_dev_staging"
+                "ui_branch": "master",
+                "api_branch": "master"
             },
             'type': 'luke',
             'gunicorn_details': {
