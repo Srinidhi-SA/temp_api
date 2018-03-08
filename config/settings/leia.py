c425caa28da503ff82108052bb061afb884505d5
ankush <ankush.patel@marlabs.com>
2018-03-08 16:25:39 +0530

    config changes for redis

--- config/settings/leia.py
+++ config/settings/leia.py
@@ -134,8 +134,8 @@ JOBSERVER_EMAIL_TEMPLATE = "Please restart jobserver- IP-"
 
 DEPLOYMENT_ENV = "prod"
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0/'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0/'
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
