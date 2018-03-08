bbaf0d700eb697839e3256cb28e44ab4958cd377
ankush <ankush.patel@marlabs.com>
2018-03-08 16:27:54 +0530

    config changes for redis

--- config/settings/luke.py
+++ config/settings/luke.py
@@ -84,7 +84,7 @@ THIS_SERVER_DETAILS = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
@@ -126,8 +126,8 @@ JOBSERVER_EMAIL_TEMPLATE = "Please restart jobserver- IP-"
 
 DEPLOYMENT_ENV = "prod"
 
-CELERY_BROKER_URL = 'redis://localhost:6379/2/'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/2/'
+CELERY_BROKER_URL = 'redis://localhost:6379/'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
