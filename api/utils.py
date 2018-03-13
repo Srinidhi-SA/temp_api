343780cd2c5c109f6f7bcd4c7b5185bea46888f2
ankush <ankush.patel@marlabs.com>
2018-03-13 15:20:04 +0530

    stockdatasets changes

--- api/utils.py
+++ api/utils.py
@@ -621,14 +621,14 @@ class StockDatasetSerializer(serializers.ModelSerializer):
         if instance.viewed == False and instance.status=='SUCCESS':
             instance.viewed = True
             instance.save()
-        try:
-            message_list = get_message(instance)
-
-            if message_list is not None:
-                message_list = [message_list[-1]]
-            ret['message'] = message_list
-        except:
-            ret['message'] = None
+        # try:
+        #     message_list = get_message(instance)
+        #
+        #     if message_list is not None:
+        #         message_list = [message_list[-1]]
+        #     ret['message'] = message_list
+        # except:
+        #     ret['message'] = None
 
         return ret
 
