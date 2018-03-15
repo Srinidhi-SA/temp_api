c731534959c9eb315d4b129b923bdeaaf402bc65
ankush <ankush.patel@marlabs.com>
2018-03-15 15:34:04 +0530

    reger chanbges

--- api/tasks.py
+++ api/tasks.py
@@ -6,7 +6,6 @@ import signal
 
 import os
 from celery.decorators import task
-from celery.contrib import rdb
 
 
 @task(name="sum_two_numbers")
@@ -105,7 +104,6 @@ def write_into_databases(job_type, object_slug, results):
         insight_object.save()
         return results
     elif job_type == "model":
-        rdb.set_trace()
         trainer_object = get_db_object(model_name=Trainer.__name__,
                                            model_slug=object_slug
                                            )
@@ -217,7 +215,6 @@ def write_into_databases1(job_type, object_slug, results):
         insight_object.save()
         return results
     elif job_type == "model":
-        rdb.set_trace()
         trainer_object = get_db_object(model_name=Trainer.__name__,
                                            model_slug=object_slug
                                            )
