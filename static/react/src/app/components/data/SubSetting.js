cfb723078d4283c6eccd24c699fa3c5c385caf7a
ArunRapolu <arun.rapolu@marlabs.com>
2018-03-29 17:27:35 +0530

    fixed #7

--- static/react/src/app/components/data/SubSetting.js
+++ static/react/src/app/components/data/SubSetting.js
@@ -126,7 +126,7 @@ export class SubSetting extends React.Component {
       });
     });
     showHideSubsetting(this.props.item.columnType, this.state.dimentionList, this.props.item.dateSuggestionFlag)
-
+    $('#saveButton').attr('disabled', true);
   }
   changeSliderValue(e) {
     //alert("coming")
@@ -527,7 +527,7 @@ export class SubSetting extends React.Component {
               </div>
               <div class="panel-footer">
                 <div class="text-right" id="saveSubSetting">
-                  <button href="javascript:void(0)" class="btn btn-alt4" id="saveButton" disabled onClick={this.saveSubSetting.bind(this)}>
+                  <button href="javascript:void(0)" class="btn btn-alt4" id="saveButton" onClick={this.saveSubSetting.bind(this)}>
                     Save
                   </button>
                 </div>
