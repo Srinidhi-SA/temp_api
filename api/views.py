ddce0d21e025ee016c0e79d1e0fe9e664594db0c
ankush <ankush.patel@marlabs.com>
2018-04-16 16:49:56 +0530

    app view queryset changes

--- api/views.py
+++ api/views.py
@@ -960,23 +960,7 @@ class AppView(viewsets.ModelViewSet):
             status="Active"
             #status__in=['SUCCESS', 'INPROGRESS']
         )
-
-        # tagsRq = self.request.query_params.get('tag', None)
-        # if tagsRq is not None:
-        #     queryset = queryset.filter(tags__icontains=tagsRq)
         app_ordered_list = copy.deepcopy(settings.APPORDERLIST)
-        # matched_and_ordered_app_list = []
-        # for app in app_ordered_list:
-        #     for app_info in queryset:
-        #         if app_info.name == app:
-        #             matched_and_ordered_app_list.append(app_info)
-        #
-        # from itertools import chain
-        # qs_none = CustomApps.objects.none()
-        # print type(queryset)
-        # print (qs_none)
-        # queryset = list(chain(qs_none, matched_and_ordered_app_list))
-        # print type(queryset)
         queryset = queryset.filter(name__in=app_ordered_list)
 
         return queryset
