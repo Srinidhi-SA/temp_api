c8e689688578575c46df9efc2bb23f8c64cfcc81
ankush <ankush.patel@marlabs.com>
2018-05-18 18:48:30 +0530

    csrf fixing

--- api/views.py
+++ api/views.py
@@ -324,7 +324,7 @@ class TrainerView(viewsets.ModelViewSet):
         return return_xml_data(xml_data, algoname)
 
     @detail_route(methods=['put'])
-    def hello(self, request, *args, **kwargs):
+    def save_hyperparameters_selected_models(self, request, *args, **kwargs):
 
         try:
             instance = self.get_object_from_all()
