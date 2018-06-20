7bdede737fc3e7f3507819d2cabec293b84fe093
ankush <ankush.patel@marlabs.com>
2018-06-20 17:06:01 +0530

    added delay to method for celery

--- api/models.py
+++ api/models.py
@@ -1609,7 +1609,7 @@ class StockDataset(models.Model):
     def create(self):
         # self.meta_data = json.dumps(dummy_audio_data_3)
         from api.tasks import stock_sense_crawl
-        stock_sense_crawl(object_slug=self.slug)
+        stock_sense_crawl.delay(object_slug=self.slug)
         # self.meta_data = self.generate_meta_data()
         # self.fake_call_mlscripts()
         # self.save()
