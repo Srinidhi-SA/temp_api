9785b2be9c8142ea4785dd10629cdb38d2a3dd17
ankush <ankush.patel@marlabs.com>
2018-06-20 17:17:57 +0530

    added inprogress in status before celery call in stock sense

--- api/models.py
+++ api/models.py
@@ -1609,6 +1609,8 @@ class StockDataset(models.Model):
     def create(self):
         # self.meta_data = json.dumps(dummy_audio_data_3)
         from api.tasks import stock_sense_crawl
+        self.status = "INPROGRESS"
+        self.save()
         stock_sense_crawl.delay(object_slug=self.slug)
         # self.meta_data = self.generate_meta_data()
         # self.fake_call_mlscripts()
