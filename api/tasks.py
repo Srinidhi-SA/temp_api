b1e462c326827157d5ffdddea48fdff5a08211b8
ankush <ankush.patel@marlabs.com>
2018-06-21 17:26:19 +0530

    stocksense mess

--- api/tasks.py
+++ api/tasks.py
@@ -421,7 +421,7 @@ def stock_sense_crawl(object_slug):
     print "got_object"*10
     stock_dataset_object.generate_meta_data()
     print "stock_sense_crawl"*2
-    metafile = open('/tmp/metafile_{0}'.format(stock_dataset_object.slug), 'w')
+    metafile = open('/tmp/metafile_{0}'.format(stock_dataset_object.slug), 'r')
     stock_dataset_object.meta_data = metafile.write(json.loads(metafile.read()))
     stock_dataset_object.fake_call_mlscripts()
     stock_dataset_object.save()
