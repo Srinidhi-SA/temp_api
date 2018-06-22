87fd4c3ef7d96cfefcf7f3d660d9b0aa2a26cbf2
ankush <ankush.patel@marlabs.com>
2018-06-22 16:45:15 +0530

    stocksense clean - using historical data

--- api/StockAdvisor/crawling/crawl_util.py
+++ api/StockAdvisor/crawling/crawl_util.py
@@ -169,7 +169,7 @@ def get_metaData(news_data):
 
 def get_required_fields(type='historical_data'):
     matching = {
-        'historical_data': ['url',  'source', 'date', 'title','desc'],
+        'historical_data': ['url',  'source', 'date'],
         'news_data': ['url',  'source', 'date', 'title','desc'],
     }
 
