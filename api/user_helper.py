88bf68cddf665f686bad9d0a87cda18034fce11a
Swati Gupta <swati.gupta@marlabs.com>
2018-07-05 21:13:51 +0530

    changes for kylo create user, added dladmin at last

--- api/user_helper.py
+++ api/user_helper.py
@@ -324,12 +324,17 @@ def create_or_update_kylo_auth_file():
         return True
     KYLO_SERVER_DETAILS = settings.KYLO_SERVER_DETAILS
     group_propertie_quote = KYLO_SERVER_DETAILS['group_propertie_quote']
-    user_properties = "dladmin=thinkbig\n\n"
-    groups_properties = "dladmin=admin,user\n\nanalyst=analyst,user\n\ndesigner=designer,user\n\noperator=operations,user\n\n"
+    user_properties = ""
+    groups_properties = ""
     all_users = User.objects.all()
     for user in all_users:
         user_properties += "{0}={1}\n\n".format(user.username, encrypt_for_kylo(user.username, user.password))
         groups_properties += "{0}={1}\n\n".format(user.username, group_propertie_quote)
+
+    #add dladmin and default users of kylo
+    user_properties +="dladmin=thinkbig"
+    groups_properties += "dladmin=admin,user\n\nanalyst=analyst,user\n\ndesigner=designer,user\n\noperator=operations,user"
+
 
     with open('/tmp/users.properties', 'w') as fp:
         fp.write(user_properties)
