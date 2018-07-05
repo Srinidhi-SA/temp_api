2555d469147823dbcbb1e58a045fd54b6ce9d336
Swati Gupta <swati.gupta@marlabs.com>
2018-07-06 01:29:01 +0530

    kylo api integration

--- api/user_helper.py
+++ api/user_helper.py
@@ -366,9 +366,11 @@ def create_or_update_kylo_auth_file():
     grps=["madvisor"]
     displayName=user.first_name+" "+user.last_name
     user_data={"displayName": displayName,"email": user.email,"enabled": True,"groups":grps,"systemName": user.username}
+    print "user_data: "+user_data
     import json
     import requests
     user_data=json.dumps(user_data)
+    print "user data after dump: "+user_data
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     r=requests.post("http://data-management-dev.marlabsai.com/proxy/v1/security/users",data=user_data,auth=('dladmin','thinkbig'),headers=headers)
-    print r.text
+    print "response from kylo: "+r.text
