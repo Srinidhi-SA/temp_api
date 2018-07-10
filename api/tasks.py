eddc47833c4a62ac4fd9a780b75c2675d02ed3d3
ankush <ankush.patel@marlabs.com>
2018-07-10 06:50:11 +0530

    changes in api

--- api/tasks.py
+++ api/tasks.py
@@ -434,4 +434,5 @@ def stock_sense_crawl(object_slug):
                                    )
     stock_dataset_object.generate_meta_data()
     stock_dataset_object.save()
+    stock_dataset_object.call_mlscripts()
     print stock_dataset_object.meta_data
