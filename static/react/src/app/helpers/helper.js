5a4da70ae61723040a57a6f0429e52cce8470ef0
Swati Gupta <swati.gupta@marlabs.com>
2018-08-02 19:17:11 +0530

    removed chatbot from login

--- static/react/src/app/helpers/helper.js
+++ static/react/src/app/helpers/helper.js
@@ -585,3 +585,24 @@ export function toggleVisualization(slug,actionsData){
 		}
 
 }
+
+export function removeChatbotOnLogout(){
+ var tags = document.getElementsByTagName('script');
+ for (var i = tags.length; i >= 0; i--){ //search backwards within nodelist for matching elements to remove
+  if (tags[i] && tags[i].getAttribute('src') != null && tags[i].getAttribute('src').indexOf("client-plugin/bot.js") != -1)
+   tags[i].parentNode.removeChild(tags[i]); //remove element by calling parentNode.removeChild()
+ }
+
+//  var chatwindow= document.getElementById("welcome-body-frame")
+//  if(chatwindow!=null)
+//  chatwindow.style.visibility="hidden"
+//
+//  var chaticon= document.getElementsByClassName("chat-icon-container")[0]
+// if(chaticon!=null)
+//  chaticon.style.visibility="hidden"
+//
+//  var chatcontainer= document.getElementsByClassName("chat-container")[0]
+//  if(chatcontainer!=null)
+//  chatcontainer.style.visibility="hidden"
+
+}
