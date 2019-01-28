c765bf4e6a48c16fecc2daea20a878d28604de2b
Vivek <vivekananda.tadala@marlabs.com>
2019-01-28 16:51:12 +0530

    FE: Cosmetic change

--- static/react/src/app/actions/appActions.js
+++ static/react/src/app/actions/appActions.js
@@ -169,7 +169,7 @@ export function createModel(modelName, targetVariable, targetLevel) {
                 }
             }
             var AlgorithmSettings = store.getState().apps.regression_algorithm_data_manual;
-            var dataCleansing = {  "coloumnSettings" : {
+            var dataCleansing = {  "columnsSettings" : {
                                     "missingValueTreatment" :store.getState().datasets.missingValueTreatment,
                                     "outlierRemoval" : store.getState().datasets.missingValueTreatment,
                                      },
@@ -181,7 +181,7 @@ export function createModel(modelName, targetVariable, targetLevel) {
                 "validationTechnique":validationTechnique,
                 "targetLevel":targetLevel,
                 "dataCleansing" : dataCleansing,
-                "featureEngineering" : {"coloumnSettings" :store.getState().datasets.featureEngineering,
+                "featureEngineering" : {"columnsSettings" :store.getState().datasets.featureEngineering,
                                        "overallSettings" :  store.getState().datasets.topLevelData,
                                           },
                 "variablesSelection":store.getState().datasets.dataPreview.meta_data.uiMetaData.varibaleSelectionArray
