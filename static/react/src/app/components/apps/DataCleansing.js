0d416d2b5c7e117fec245cdd46ca70faf486d73a
VidyaR13 <vidya.ramaiah@marlabs.com>
2019-02-06 16:22:22 +0530

    DC:No. of missing values displayed

--- static/react/src/app/components/apps/DataCleansing.js
+++ static/react/src/app/components/apps/DataCleansing.js
@@ -175,7 +175,7 @@ getMissingValueTreatmentOptions(dataType, colName, colSlug){
                     <option value={item.name} selected >{item.displayName}</option>))
 
     var selectedValue = "none";
-    if(colSlug  in this.props.datasets.missingValueTreatment){
+    if(colSlug in this.props.datasets.missingValueTreatment){
         selectedValue = this.props.datasets.missingValueTreatment[colSlug].treatment
     }
 
@@ -233,7 +233,7 @@ getMissingValueTreatmentOptions(dataType, colName, colSlug){
          </td>
          <td>
              {item.columnStats.filter(function(items){
-                 return  items.name == "PercentageMissingValue" }).map((option)=>{
+                 return  items.name == "numberOfNulls" }).map((option)=>{
                    return(<span>{option.value}</span>);
                }
                )}
@@ -306,7 +306,7 @@ getMissingValueTreatmentOptions(dataType, colName, colSlug){
                             <th><b>Data type</b></th>
                             <th><b>No of unique values</b></th>
                             <th><b>No of outliers</b></th>
-                            <th><b>% of missing values</b></th>
+                            <th><b>No of missing values</b></th>
                             <th><b>Missing value treatment</b></th>
                             <th><b>Outlier removal</b></th>
                             </tr>
