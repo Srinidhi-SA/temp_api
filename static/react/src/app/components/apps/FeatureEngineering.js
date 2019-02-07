c651c83078226151f4874122164e2a8a65d292b6
SuryatejaS <sunkara.surya@marlabs.com>
2019-02-08 00:36:45 +0530

    FE:For DataType first letter Changed to UpperCase

--- static/react/src/app/components/apps/FeatureEngineering.js
+++ static/react/src/app/components/apps/FeatureEngineering.js
@@ -219,7 +219,7 @@ validateTransformdata(){
        return (
                <tr key={key}>
                   <td className="text-left"> {item.name}</td>
-                  <td> {item.columnType}</td>
+                  <td> {item.columnType.charAt(0).toUpperCase()+item.columnType.slice(1)}</td>
                   <td> <Button onClick={this.openBinsOrLevelsModal.bind(this, item)} disabled={this.isBinningOrLevelsDisabled(item)} bsStyle="primary">CREATE BINS OR LEVELS</Button></td>
                   <td> <Button onClick={this.openTransformColumnModal.bind(this,item)} bsStyle="primary">TRANSFORM</Button></td>
                </tr>  );
