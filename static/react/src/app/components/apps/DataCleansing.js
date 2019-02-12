8d830fa7c4d195717ba1d21360112dd317130f7e
VidyaR13 <vidya.ramaiah@marlabs.com>
2019-02-12 11:29:11 +0530

    --

--- static/react/src/app/components/apps/DataCleansing.js
+++ static/react/src/app/components/apps/DataCleansing.js
@@ -68,8 +68,8 @@ export class DataCleansing extends React.Component {
 
   componentWillMount() {
     if(this.props.apps_regression_modelName == "" || this.props.currentAppDetails == null){
-            window.history.go(-1);
-        }
+      window.history.go(-1);
+    }
     if (this.props.dataPreview == null || isEmpty(this.props.dataPreview) || this.props.dataPreview.status == 'FAILED') {
       this.props.dispatch(getDataSetPreview(this.props.match.params.slug));
     }else{
@@ -77,12 +77,12 @@ export class DataCleansing extends React.Component {
     }
 
     var proccedUrl = this.props.match.url.replace('dataCleansing','featureEngineering');
-
     if(this.props.match.path.includes("slug")){
       this.buttons['proceed']={
         url : proccedUrl,
-        text:"Proceed"};
-      }
+        text:"Proceed"
+      };
+    }
   }
 
   componentDidMount() {
@@ -92,99 +92,89 @@ export class DataCleansing extends React.Component {
   }
 
   shouldComponentUpdate(nextProps) {
-
     return true;
   }
 
-onchangeMissingValueTreatment(event, variable_name){
+  onchangeMissingValueTreatment(event, variable_name){
+  }
 
-}
+  missingValueTreatmentOnChange(event){
+    console.log(event.target.dataset);
+    this.props.dispatch(missingValueTreatmentSelectedAction(event.target.dataset["colname"],event.target.dataset["coltype"], event.target.dataset["colslug"], event.target.value));
+  }
 
+  outlierRemovalOnChange(event){
+    this.props.dispatch(outlierRemovalSelectedAction(event.target.dataset["colname"],event.target.dataset["coltype"],event.target.dataset["colslug"], event.target.value));
+  }
 
-missingValueTreatmentOnChange(event){
+  variableCheckboxOnChange(event){
+    this.props.dispatch(variableSelectedAction(event.target.dataset["colslug"], event.target.checked));
+  }
 
-  console.log(event.target.dataset);
-  this.props.dispatch(missingValueTreatmentSelectedAction(event.target.dataset["colname"],event.target.dataset["coltype"], event.target.dataset["colslug"], event.target.value));
+  handleDuplicateAttributesOnChange(event){
+    this.setState({value1: event.target.value})
+    this.props.dispatch(removeDuplicateAttributesAction(event.target.name,event.target.value));
+  }
 
-}
-outlierRemovalOnChange(event){
-  this.props.dispatch(outlierRemovalSelectedAction(event.target.dataset["colname"],event.target.dataset["coltype"],event.target.dataset["colslug"], event.target.value));
-}
+  handleDuplicateObservationsOnChange(event){
+    this.setState({value2: event.target.value})
+    this.props.dispatch(removeDuplicateObservationsAction(event.target.name,event.target.value));
+  }
 
-variableCheckboxOnChange(event){
-  this.props.dispatch(variableSelectedAction(event.target.dataset["colslug"], event.target.checked));
-}
+  handleDataTypeChange(colSlug, event){
+    this.props.dispatch(dataCleansingDataTypeChange(colSlug, event.target.value));
+  }
 
-handleDuplicateAttributesOnChange(event){
-  this.setState({value1: event.target.value})
-  this.props.dispatch(removeDuplicateAttributesAction(event.target.name,event.target.value));
-}
+  getUpdatedDataType(colSlug){
+    // this.props.dataPreview.meta_data.uiMetaData.columnDataUI.filter(item => item.slug == slug);
+    // console.log(this.props.dataPreview.meta_data.uiMetaData.columnDataUI.filter(item => item.slug == slug));
+    let colType = this.props.dataPreview.meta_data.uiMetaData.columnDataUI.filter(item => item.slug == colSlug)[0].columnType
+    var arr = [ "Measure","Dimension", "Datetime"]
+    var optionsHtml = arr.map(item => {
+      if(item.toLowerCase()== colType.toLowerCase() ){
+        return <option value={item.toLowerCase()} selected>{item}</option>
+      }else{
+        return <option value={item.toLowerCase()} >{item}</option>
+      }
+    })
+    return <select className="form-control"  onChange={this.handleDataTypeChange.bind(this,colSlug )} > {optionsHtml} </select>
+  }
 
-handleDuplicateObservationsOnChange(event){
-  this.setState({value2: event.target.value})
-  this.props.dispatch(removeDuplicateObservationsAction(event.target.name,event.target.value));
-}
+  proceedFeatureEngineering()
+  {
+    var proccedUrl = this.props.match.url.replace('dataCleansing','featureEngineering');
+    this.props.history.push(proccedUrl);
+  }
 
-handleDataTypeChange(colSlug, event){
-  this.props.dispatch(dataCleansingDataTypeChange(colSlug, event.target.value));
-}
-getUpdatedDataType(colSlug){
-  // this.props.dataPreview.meta_data.uiMetaData.columnDataUI.filter(item => item.slug == slug);
-  // console.log(this.props.dataPreview.meta_data.uiMetaData.columnDataUI.filter(item => item.slug == slug));
-  let colType = this.props.dataPreview.meta_data.uiMetaData.columnDataUI.filter(item => item.slug == colSlug)[0].columnType
-  var arr = [ "Measure","Dimension", "Datetime"]
-  var optionsHtml = arr.map(item => {
-    if(item.toLowerCase()== colType.toLowerCase() ){
-      return <option value={item.toLowerCase()} selected>{item}</option>
-    }else{
-      return <option value={item.toLowerCase()} >{item}</option>
-    }
-  })
-  return <select className="form-control"  onChange={this.handleDataTypeChange.bind(this,colSlug )} >
-  {optionsHtml}
-</select>
-}
-
-proceedFeatureEngineering()
-{
-  var proccedUrl = this.props.match.url.replace('dataCleansing','featureEngineering');
-  this.props.history.push(proccedUrl);
-}
-
-getOutlierRemovalOptions(dataType, colName, colSlug){
-  var data_cleansing = this.props.dataPreview.meta_data.uiMetaData.fe_config.data_cleansing ;
-  if (dataType in data_cleansing && "outlier_removal" in data_cleansing[dataType]){
-    var dcHTML =  (data_cleansing[dataType].outlier_removal.operations.map(item =>
-                        <option value={item.name} selected >{item.displayName}</option>))
-
-    var selectedValue = "none";
-    if(colSlug in this.props.datasets.outlierRemoval){
+  getOutlierRemovalOptions(dataType, colName, colSlug){
+    var data_cleansing = this.props.dataPreview.meta_data.uiMetaData.fe_config.data_cleansing ;
+    if (dataType in data_cleansing && "outlier_removal" in data_cleansing[dataType]){
+      var dcHTML =  (data_cleansing[dataType].outlier_removal.operations.map(item => <option value={item.name} selected >{item.displayName}</option>))
+      var selectedValue = "none";
+      if(colSlug in this.props.datasets.outlierRemoval){
         selectedValue = this.props.datasets.outlierRemoval[colSlug].treatment
+      }
+      return (
+        <select className="form-control" data-coltype={dataType} data-colName={colName} data-colslug={colSlug} onChange={this.outlierRemovalOnChange.bind(this)} value={selectedValue} >{dcHTML}</select>
+      );
     }
-
-
-    return (<select className="form-control" data-coltype={dataType} data-colName={colName} data-colslug={colSlug} onChange={this.outlierRemovalOnChange.bind(this)} value={selectedValue} >{dcHTML}</select>);
+    else return ""
   }
-  else { return "";}
-}
 
-getMissingValueTreatmentOptions(dataType, colName, colSlug){
-  var data_cleansing = this.props.dataPreview.meta_data.uiMetaData.fe_config.data_cleansing ;
-  if (dataType in data_cleansing && "missing_value_treatment" in data_cleansing[dataType]){
-    var dcHTML =  (data_cleansing[dataType].missing_value_treatment.operations.map(item =>
-                    <option value={item.name} selected >{item.displayName}</option>))
-
-    var selectedValue = "none";
-    if(colSlug in this.props.datasets.missingValueTreatment){
+  getMissingValueTreatmentOptions(dataType, colName, colSlug){
+    var data_cleansing = this.props.dataPreview.meta_data.uiMetaData.fe_config.data_cleansing ;
+    if (dataType in data_cleansing && "missing_value_treatment" in data_cleansing[dataType]){
+      var dcHTML =  (data_cleansing[dataType].missing_value_treatment.operations.map(item => <option value={item.name} selected >{item.displayName}</option>))
+      var selectedValue = "none";
+      if(colSlug in this.props.datasets.missingValueTreatment){
         selectedValue = this.props.datasets.missingValueTreatment[colSlug].treatment
+      }
+      return (
+        <select className="form-control" data-coltype={dataType}  data-colslug={colSlug} data-colname={colName} onChange={this.missingValueTreatmentOnChange.bind(this)} value={selectedValue} >{dcHTML}</select>
+      );
     }
-
-    return (<select className="form-control" data-coltype={dataType}  data-colslug={colSlug} data-colname={colName} onChange={this.missingValueTreatmentOnChange.bind(this)} value={selectedValue} >{dcHTML}</select>);
+    else { return "";}
   }
-  else { return "";}
-}
-
-
 
   render() {
     var cleansingHtml = <span>"Loading ... "</span>;
@@ -193,124 +183,112 @@ getMissingValueTreatmentOptions(dataType, colName, colSlug){
       var data_cleansing = this.props.dataPreview.meta_data.uiMetaData.fe_config.data_cleansing ;
       var removedVariables = getRemovedVariableNames(this.props.datasets);
       cleansingHtml = this.props.dataPreview.meta_data.scriptMetaData.columnData.map(item => {
-        // console.log(item);
-        //
-        //
-        if(removedVariables.indexOf(item.name)!= -1|| item.ignoreSuggestionFlag) return "";
-
+        if(removedVariables.indexOf(item.name)!= -1|| item.ignoreSuggestionFlag)
+          return "";
         return (
           <tr>
-            <td><div class="ma-checkbox inline">
-                <input id={item.slug} type="checkbox" class="needsclick variableToBeSelected"  data-colslug={item.slug} onChange={this.variableCheckboxOnChange.bind(this)}/>
-                <label for={item.slug}> </label>
-              </div></td>
-
-          <td className="text-left">{item.name}</td>
-          <td>  {this.getUpdatedDataType(item.slug)}</td>
-         {/* using filter and map to retrive data from array inside array*/}
-         {/* <td> */}
-         <td>
-             {item.columnStats.filter(function(items){
-                 return  items.name == "numberOfUniqueValues" }).map((option)=>{
-                   return(<span>{option.value}</span>);
+            <td>
+              <div class="ma-checkbox inline">
+              <input id={item.slug} type="checkbox" class="needsclick variableToBeSelected"  data-colslug={item.slug} onChange={this.variableCheckboxOnChange.bind(this)}/>
+              <label for={item.slug}> </label>
+              </div>
+            </td>
+            <td className="text-left">{item.name}</td>
+            <td>  {this.getUpdatedDataType(item.slug)}</td>
+            <td>
+              {item.columnStats.filter(function(items){
+                return  items.name == "numberOfUniqueValues" }).map((option)=>{
+                  return(<span>{option.value}</span>);
+                }
+              )}
+            </td>
+            <td>
+              {item.columnStats.filter(function(items){
+                return  items.name == "Outliers" }).map((option)=>{
+                  return(<span>{option.value}</span>);
+                }
+              )}
+            </td>
+            <td>
+              {item.columnStats.filter(function(items){
+                return  items.name == "numberOfNulls" }).map((option)=>{
+                  return(<span>{option.value}</span>);
                }
                )}
-         </td>
-         <td>
-             {item.columnStats.filter(function(items){
-                 return  items.name == "Outliers" }).map((option)=>{
-                   return(<span>{option.value}</span>);
-               }
-               )}
-         </td>
-         <td>
-             {item.columnStats.filter(function(items){
-                 return  items.name == "numberOfNulls" }).map((option)=>{
-                   return(<span>{option.value}</span>);
-               }
-               )}
-         </td>
-         <td>
-              {this.getMissingValueTreatmentOptions(item.columnType, item.name, item.slug)}
-         </td>
-         <td>
-              {this.getOutlierRemovalOptions(item.columnType, item.name, item.slug)}
-         </td>
+             </td>
+             <td> {this.getMissingValueTreatmentOptions(item.columnType, item.name, item.slug)} </td>
+             <td> {this.getOutlierRemovalOptions(item.columnType, item.name, item.slug)} </td>
+           </tr>
+         );
+       })
+     }
 
-          </tr>
-        );
-      })
-    }
-
-    return (
-        // <!-- Main Content starts with side-body -->
+     return (
+       // <!-- Main Content starts with side-body -->
        <div className="side-body">
-        {/* <!-- Page Title and Breadcrumbs -->*/}
-        <div className="page-head">
-          <h3 className="xs-mt-0 xs-mb-0 text-capitalize"> Data Cleansing</h3>
-        </div>
-        {/*<!-- /.Page Title and Breadcrumbs -->*/}
-        {/*<!-- Page Content Area -->*/}
+         {/* <!-- Page Title and Breadcrumbs -->*/}
+         <div className="page-head">
+           <h3 className="xs-mt-0 xs-mb-0 text-capitalize"> Data Cleansing</h3>
+         </div>
+         {/*<!-- /.Page Title and Breadcrumbs -->*/}
+         {/*<!-- Page Content Area -->*/}
          <div className="main-content">
-         <div className="row">
-          <div className="col-md-12">
-          <div className="panel box-shadow xs-m-0">
-            <div className="panel-body no-border xs-p-20">
-              <div className="form-group">
-                <label for="rd1" class="col-sm-5 control-label"><i class="fa fa-angle-double-right"></i> Do you want to remove duplicate attributes/columns in the dataset?</label>
-                <div className="col-sm-7">
-                  <div className="content-section implementation">
-                     <InputSwitch id="rd1" onLabel="Yes" offLabel="No"  checked={this.state.value1}  name="remove_duplicate_attributes"  onChange={this.handleDuplicateAttributesOnChange.bind(this)} />
-                  </div>
-                  </div>
-              </div>
-              <div className="clearfix xs-mb-5"></div>
-              <div className="form-group">
-                <label for="rd2" class="col-sm-5 control-label"><i class="fa fa-angle-double-right"></i> Do you want to remove duplicate observations  in the dataset?</label>
-                <div className="col-sm-7">
-                  <div className="content-section implementation">
-                    <InputSwitch id="rd2" checked={this.state.value2}  name="remove_duplicate_observations" onChange={this.handleDuplicateObservationsOnChange.bind(this)} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-                <div className="panel box-shadow ">
-                    <div class="panel-body no-border xs-p-20">
-                  <div className="table-responsive ">
-                      <table className="table table-striped table-bordered break-if-longText">
-                        <thead>
-                          <tr>
-                            {/* <th> <div class="ma-checkbox inline">
-                                <input id="checkAll" type="checkbox" class="needsclick" onChange={this.handleSelectAll.bind(this)}/>
-                                <label for="checkAll">All</label>
-                              </div>
-                            </th> */}
-
-                            <th className="text-left"><b>Variable name</b></th>
-                            <th><b>Data type</b></th>
-                            <th><b>No of unique values</b></th>
-                            <th><b>No of outliers</b></th>
-                            <th><b>No of missing values</b></th>
-                            <th><b>Missing value treatment</b></th>
-                            <th><b>Outlier removal</b></th>
-                            </tr>
-                        </thead>
-                        <tbody className="no-border-x">
-                          {cleansingHtml}
-                        </tbody>
-                      </table>
-
-                  </div>
-  <div class="buttonRow text-right">
-     <Button onClick={this.proceedFeatureEngineering.bind(this)}  bsStyle="primary">Proceed <i class="fa fa-angle-double-right"></i></Button>
-   </div>
- </div>
-        </div>
-      </div>
-      {/* <!--End of Page Content Area --> */}
-    </div>
-    // <!-- /. Main Content ends with side-body -->
-    );
-  }
-}
+           <div className="row">
+             <div className="col-md-12">
+               <div className="panel box-shadow xs-m-0">
+                 <div className="panel-body no-border xs-p-20">
+                   <div className="form-group">
+                     <label for="rd1" class="col-sm-5 control-label"><i class="fa fa-angle-double-right"></i> Do you want to remove duplicate attributes/columns in the dataset?</label>
+                     <div className="col-sm-7">
+                       <div className="content-section implementation">
+                         <InputSwitch id="rd1" onLabel="Yes" offLabel="No"  checked={this.state.value1}  name="remove_duplicate_attributes"  onChange={this.handleDuplicateAttributesOnChange.bind(this)} />
+                       </div>
+                     </div>
+                   </div>
+                   <div className="clearfix xs-mb-5"></div>
+                   <div className="form-group">
+                      <label for="rd2" class="col-sm-5 control-label"><i class="fa fa-angle-double-right"></i> Do you want to remove duplicate observations  in the dataset?</label>
+                      <div className="col-sm-7">
+                        <div className="content-section implementation">
+                          <InputSwitch id="rd2" checked={this.state.value2}  name="remove_duplicate_observations" onChange={this.handleDuplicateObservationsOnChange.bind(this)} />
+                        </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+               <div className="panel box-shadow ">
+                 <div class="panel-body no-border xs-p-20">
+                   <div className="table-responsive ">
+                     <table className="table table-striped table-bordered break-if-longText">
+                       <thead>
+                         <tr>
+                           {/* <th>
+                             <div class="ma-checkbox inline">
+                               <input id="checkAll" type="checkbox" class="needsclick" onChange={this.handleSelectAll.bind(this)}/>
+                               <label for="checkAll">All</label>
+                             </div>
+                           </th> */}
+                           <th className="text-left"><b>Variable name</b></th>
+                           <th><b>Data type</b></th>
+                           <th><b>No of unique values</b></th>
+                           <th><b>No of outliers</b></th>
+                           <th><b>No of missing values</b></th>
+                           <th><b>Missing value treatment</b></th>
+                           <th><b>Outlier removal</b></th>
+                         </tr>
+                       </thead>
+                       <tbody className="no-border-x"> {cleansingHtml} </tbody>
+                     </table>
+                   </div>
+                   <div class="buttonRow text-right"><Button onClick={this.proceedFeatureEngineering.bind(this)}  bsStyle="primary">Proceed <i class="fa fa-angle-double-right"></i></Button></div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           {/* <!-- /. Main Content ends with side-body --> */}
+         </div>
+         {/* <!--End of Page Content Area --> */}
+       </div>
+     );
+   }
+ }
