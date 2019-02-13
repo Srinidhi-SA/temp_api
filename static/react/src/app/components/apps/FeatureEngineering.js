2015b6f0ce706526ab441a968513cf06fd7769ff
VidyaR13 <vidya.ramaiah@marlabs.com>
2019-02-13 12:33:11 +0530

    FE changes

--- static/react/src/app/components/apps/FeatureEngineering.js
+++ static/react/src/app/components/apps/FeatureEngineering.js
@@ -188,23 +188,14 @@ export class FeatureEngineering extends React.Component {
             $("#fileErrorMsg").html("Start Date should be before End Date");
           }
         }
-
-   var dataToSave = JSON.parse(JSON.stringify(this.state[this.props.selectedItem.slug][actionType]));
-    this.props.dispatch(saveBinLevelTransformationValuesAction(this.props.selectedItem.slug, actionType, dataToSave));
-    this.closeBinsOrLevelsModal();
-    this.closeTransformColumnModal();
-
-
-}
-
-
-
+        var dataToSave = JSON.parse(JSON.stringify(this.state[this.props.selectedItem.slug][actionType]));
+        this.props.dispatch(saveBinLevelTransformationValuesAction(this.props.selectedItem.slug, actionType, dataToSave));
+        this.closeBinsOrLevelsModal();
+        this.closeTransformColumnModal();
       }
+    }
   }
-
-
-validateTransformdata(){
-}
+  validateTransformdata(){ }
 
   handleTopLevelRadioButtonOnchange(event){
     this.state.topLevelRadioButton = event.target.value;
@@ -226,7 +217,7 @@ validateTransformdata(){
       return ((this.state.topLevelRadioButton == "true" && item.columnType == "measure") || (item.columnType!=item.actualColumnType)  )
   }
 
- render() {
+  render() {
     console.log("FeatureEngineering render method is called...");
     var feHtml = "";
     var binsOrLevelsPopup = "";
@@ -239,149 +230,132 @@ validateTransformdata(){
     var numberOfSelectedMeasures = 0;
     var numberOfSelectedDimensions = 0;
 
-        if (this.props.dataPreview != null) {
-            feHtml = this.props.dataPreview.meta_data.scriptMetaData.columnData.map((item,key )=> {
-            if(removedVariables.indexOf(item.name)!= -1|| item.ignoreSuggestionFlag) return "";
+    if (this.props.dataPreview != null) {
+      feHtml = this.props.dataPreview.meta_data.scriptMetaData.columnData.map((item,key )=> {
+        if(removedVariables.indexOf(item.name)!= -1|| item.ignoreSuggestionFlag) return "";
         if(item.columnType == "measure") numberOfSelectedMeasures +=1;
         else numberOfSelectedDimensions +=1;
-       return (
-               <tr key={key}>
-                  <td className="text-left"> {item.name}</td>
-
-                  <td> {item.columnType.charAt(0).toUpperCase()+item.columnType.slice(1)}</td>
-                  <td> <Button onClick={this.openBinsOrLevelsModal.bind(this, item)} disabled={this.isBinningOrLevelsDisabled(item)} bsStyle="cst_button">Create Bins or Levels</Button></td>
-                  <td> <Button onClick={this.openTransformColumnModal.bind(this,item)} bsStyle="cst_button">Transform</Button></td>
-
-               </tr>  );
-              })
-            }
-            if(this.props.selectedItem.columnType == "measure"){
-              binOrLevels=   <Bins parentPickValue={this.pickValue}   clearBinsAndIntervals={this.clearBinsAndIntervals} />
-              binOrLevelData="binData";
-            }
-            else if(this.props.selectedItem.columnType == "dimension")
-            {
-              binOrLevels= <Levels parentPickValue={this.pickValue} parentUpdateLevelsData={this.updateLevelsData} levelsData={this.getLevelsData()}/>
-              binOrLevelData="levelData";
-            }
-            else
-            {
-              // binOrLevels=""
-              binOrLevels= <Levels parentPickValue={this.pickValue} parentUpdateLevelsData={this.updateLevelsData} levelsData={this.getLevelsData()}/>
-              binOrLevelData="levelData";
-            }
-
-
-
-  binsOrLevelsPopup =
-      (
-        // <div class="col-md-3 xs-mb-15 list-boxes" >
-
-            <div id="binsOrLevels" role="dialog" className="modal fade modal-colored-header">
-              <Modal show={this.props.binsOrLevelsShowModal} onHide={this.closeBinsOrLevelsModal.bind(this)} dialogClassName="modal-colored-header modal-md" style={{overflow: 'inherit' }} >
-
-                <Modal.Header closeButton>
-                  <h3 className="modal-title">Create { (this.props.selectedItem.columnType == "measure")? "Bins" : "Levels" }</h3>
-                </Modal.Header>
-                <Modal.Body>
-                <div>
-                    <h4>What you want to do?</h4>
-                          {binOrLevels}
-                </div>
-                <div id="errorMsgs" className="text-danger"></div>
-                </Modal.Body>
-                <Modal.Footer>
-                <Button onClick={this.closeBinsOrLevelsModal.bind(this)}>Cancel</Button>
-                <Button bsStyle="primary" form="binsForm" content="Submit" value="Submit" onClick={this.handleCreateClicked.bind(this, binOrLevelData)}>Create</Button>
-              </Modal.Footer>
-            </Modal>
-          </div>
-        // </div>
-      )
-    transformColumnPopup = (
-      <div class="col-md-3 xs-mb-15 list-boxes" >
-        <div id="transformColumnPopup" role="dialog" className="modal fade modal-colored-header">
-          <Modal show={this.props.transferColumnShowModal} onHide={this.closeTransformColumnModal.bind(this)} dialogClassName="modal-colored-header">
+        return (
+          <tr key={key}>
+            <td className="text-left"> {item.name}</td>
+            <td> {item.columnType.charAt(0).toUpperCase()+item.columnType.slice(1)}</td>
+            <td> <Button onClick={this.openBinsOrLevelsModal.bind(this, item)} disabled={this.isBinningOrLevelsDisabled(item)} bsStyle="cst_button">Create Bins or Levels</Button></td>
+            <td> <Button onClick={this.openTransformColumnModal.bind(this,item)} bsStyle="cst_button">Transform</Button></td>
+          </tr>);
+        })
+      }
+      if(this.props.selectedItem.columnType == "measure"){
+        binOrLevels=   <Bins parentPickValue={this.pickValue} clearBinsAndIntervals={this.clearBinsAndIntervals} />
+        binOrLevelData="binData";
+      }
+      else if(this.props.selectedItem.columnType == "dimension")
+      {
+        binOrLevels= <Levels parentPickValue={this.pickValue} parentUpdateLevelsData={this.updateLevelsData} levelsData={this.getLevelsData()}/>
+        binOrLevelData="levelData";
+      }
+      else
+      {
+        binOrLevels= <Levels parentPickValue={this.pickValue} parentUpdateLevelsData={this.updateLevelsData} levelsData={this.getLevelsData()}/>
+        binOrLevelData="levelData";
+      }
+      binsOrLevelsPopup = (
+        <div id="binsOrLevels" role="dialog" className="modal fade modal-colored-header">
+          <Modal show={this.props.binsOrLevelsShowModal} onHide={this.closeBinsOrLevelsModal.bind(this)} dialogClassName="modal-colored-header modal-md" style={{overflow: 'inherit' }} >
             <Modal.Header closeButton>
-              <h3 className="modal-title">Transform column</h3>
+              <h3 className="modal-title">Create { (this.props.selectedItem.columnType == "measure")? "Bins" : "Levels" }</h3>
             </Modal.Header>
             <Modal.Body>
-              {<Transform parentPickValue={this.pickValue} />}
+              <div>
+                <h4>What you want to do?</h4>
+                {binOrLevels}
+              </div>
+              <div id="errorMsgs" className="text-danger"></div>
             </Modal.Body>
             <Modal.Footer>
-              <Button onClick={this.closeTransformColumnModal.bind(this)}>Cancel</Button>
-              <Button bsStyle="primary" onClick={this.handleCreateClicked.bind(this,"transformationData")}>Create</Button>
+              <Button onClick={this.closeBinsOrLevelsModal.bind(this)}>Cancel</Button>
+              <Button bsStyle="primary" form="binsForm" content="Submit" value="Submit" onClick={this.handleCreateClicked.bind(this, binOrLevelData)}>Create</Button>
             </Modal.Footer>
           </Modal>
         </div>
-      </div>
-    )
-    return (
-     // <!-- Main Content starts with side-body -->
-    <div className="side-body">
-        {/* <!-- Page Title and Breadcrumbs --> */}
-      <div class="page-head">
-          <h3 class="xs-mt-0 xs-mb-0 text-capitalize">Feature Engineering</h3>
-      </div>
-        {/*<!-- Page Content Area -->*/}
-        {/*<!-- /.Page Title and Breadcrumbs -->*/}
-        {binsOrLevelsPopup}
-        {transformColumnPopup}
-       <div className="main-content">
-        <div class="row">
-          <div class="col-md-12">
-           <div class="panel box-shadow xs-m-0">
-             <div class="panel-body no-border xs-p-20">
-               <h4> The dataset contains {numberOfSelectedMeasures + numberOfSelectedDimensions} columns or features ({numberOfSelectedMeasures} measures and {numberOfSelectedDimensions} dimensions).  If you would like to transform the existing features or
-                create new features from the existing data, you can use the options provided below. </h4>
-		                 <hr/>
-                     <p class="inline-block">
-                       <i class="fa fa-angle-double-right text-primary"></i> Do you want to convert all measures into dimension using binning? &nbsp;&nbsp;&nbsp;
-                     </p>
-                     <span onChange={this.handleTopLevelRadioButtonOnchange.bind(this)} className="inline">
-                       <div class="ma-checkbox inline">
-                         <input type="radio" id="mTod-binning1" value="true" name="mTod-binning"  checked={this.state.topLevelRadioButton === "true"} />
-                         <label for="mTod-binning1">Yes</label>
-                       </div>
-                       <div class="ma-checkbox inline">
-                         <input type="radio" id="mTod-binning2" value="false" name="mTod-binning"  checked={this.state.topLevelRadioButton === "false"} />
-                         <label for="mTod-binning2">No </label>
-                       </div>
-                     </span>
-            {(this.state.topLevelRadioButton == "true")?<div id="box-binning" class="xs-ml-20 block-inline"   >
-              <span class="inline-block"> Number of bins : <input type="number" onInput={this.handleTopLevelInputOnchange.bind(this)} class="test_css form-control" maxlength="2" id="flight_number" name="number"/></span></div>:""}
-
+      )
+      transformColumnPopup = (
+        <div class="col-md-3 xs-mb-15 list-boxes" >
+          <div id="transformColumnPopup" role="dialog" className="modal fade modal-colored-header">
+            <Modal show={this.props.transferColumnShowModal} onHide={this.closeTransformColumnModal.bind(this)} dialogClassName="modal-colored-header">
+              <Modal.Header closeButton>
+                <h3 className="modal-title">Transform column</h3>
+              </Modal.Header>
+              <Modal.Body>
+                <Transform parentPickValue={this.pickValue}/>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.closeTransformColumnModal.bind(this)}>Cancel</Button>
+                <Button bsStyle="primary" onClick={this.handleCreateClicked.bind(this,"transformationData")}>Create</Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </div>
-
-        <div className="panel box-shadow ">
-        <div class="panel-body no-border xs-p-20">
-         <div className="table-responsive ">
-          <table className="table table-striped table-bordered break-if-longText">
-            <thead>
-              <tr key="trKey">
-                <th className="text-left"><b>Variable name</b></th>
-                <th ><b>Data type</b></th>
-                <th></th>
-                <th></th>
-              </tr>
-                </thead>
-                  <tbody className="no-border-x">{feHtml}</tbody>
-                </table>
+      )
+      return (
+        // <!-- Main Content starts with side-body -->
+        <div className="side-body">
+          {/* <!-- Page Title and Breadcrumbs --> */}
+          <div class="page-head">
+            <h3 class="xs-mt-0 xs-mb-0 text-capitalize">Feature Engineering</h3>
           </div>
-		               <div className="buttonRow text-right" id="dataPreviewButton">
-                     <Button onClick={this.handleProcedClicked.bind(this)} bsStyle="primary">{this.buttons.proceed.text} <i class="fa fa-angle-double-right"></i></Button>
+          {/*<!-- Page Content Area -->*/}
+          {/*<!-- /.Page Title and Breadcrumbs -->*/}
+          {binsOrLevelsPopup}
+          {transformColumnPopup}
+          <div className="main-content">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="panel box-shadow xs-m-0">
+                  <div class="panel-body no-border xs-p-20">
+                    <h4> The dataset contains {numberOfSelectedMeasures + numberOfSelectedDimensions} columns or features ({numberOfSelectedMeasures} measures and {numberOfSelectedDimensions} dimensions).  If you would like to transform the existing features or create new features from the existing data, you can use the options provided below. </h4><hr/>
+                    <p class="inline-block">
+                      <i class="fa fa-angle-double-right text-primary"></i> Do you want to convert all measures into dimension using binning? &nbsp;&nbsp;&nbsp;
+                    </p>
+                    <span onChange={this.handleTopLevelRadioButtonOnchange.bind(this)} className="inline">
+                      <div class="ma-checkbox inline">
+                        <input type="radio" id="mTod-binning1" value="true" name="mTod-binning"  checked={this.state.topLevelRadioButton === "true"} />
+                        <label for="mTod-binning1">Yes</label>
+                      </div>
+                      <div class="ma-checkbox inline">
+                        <input type="radio" id="mTod-binning2" value="false" name="mTod-binning"  checked={this.state.topLevelRadioButton === "false"} />
+                        <label for="mTod-binning2">No </label>
+                      </div>
+                    </span>
+                    {(this.state.topLevelRadioButton == "true")?<div id="box-binning" class="xs-ml-20 block-inline"><span class="inline-block"> Number of bins : <input type="number" onInput={this.handleTopLevelInputOnchange.bind(this)} class="test_css form-control" maxlength="2" id="flight_number" name="number"/></span></div>:""}
+                  </div>
+                </div>
+                <div className="panel box-shadow ">
+                  <div class="panel-body no-border xs-p-20">
+                    <div className="table-responsive ">
+                      <table className="table table-striped table-bordered break-if-longText">
+                        <thead>
+                          <tr key="trKey">
+                            <th className="text-left"><b>Variable name</b></th>
+                            <th ><b>Data type</b></th>
+                            <th></th>
+                            <th></th>
+                          </tr>
+                        </thead>
+                        <tbody className="no-border-x">{feHtml}</tbody>
+                      </table>
                     </div>
-        </div>
-        </div>
-
+                    <div className="buttonRow text-right" id="dataPreviewButton">
+                      <Button onClick={this.handleProcedClicked.bind(this)} bsStyle="primary">{this.buttons.proceed.text} <i class="fa fa-angle-double-right"></i></Button>
                     </div>
-{/* <!--End of Page Content Area --> */}
-      </div>
-  </div>
-        {/* <!-- Main Content ends with side-body --> */}
-      </div>
-    );
+                  </div>
+                </div>
+              </div>
+              {/* <!--End of Page Content Area --> */}
+            </div>
+          </div>
+          {/* <!-- Main Content ends with side-body --> */}
+        </div>
+      );
     }
 
   openBinsOrLevelsModal(item) {
@@ -391,30 +365,30 @@ validateTransformdata(){
 
   closeBinsOrLevelsModal(event) {
     console.log("closeddddd ---closeBinsOrLevelsModal");
-    //debugger;
     console.log(".... ",);
     this.props.dispatch(closeBinsOrLevelsModalAction());
-
   }
 
   openTransformColumnModal(item) {
     console.log("open ---openTransformColumnModal");
     this.props.dispatch(openTransformColumnModalAction(item));
   }
+
   closeTransformColumnModal() {
     console.log("closeddddd ---closeTransformColumnModal");
     this.props.dispatch(closeTransformColumnModalAction());
   }
+
   handleSelect(selectedKey) {
     console.log(`selected ${selectedKey}`);
     this.props.dispatch(selectedBinsOrLevelsTabAction(selectedKey));
   }
+
   // createBins(slug,actionType,userData) {
   // this.props.dispatch(saveBinLevelTransformationValuesAction(slug, actionType, userData);
   // }
 
-    createLevels(slug) {
-      this.props.dispatch();
-    }
-
+  createLevels(slug) {
+    this.props.dispatch();
+  }
 }
