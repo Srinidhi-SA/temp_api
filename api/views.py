f114b2e2290af33d02db1ce1a95ed40ee8bde5c6
ankushmarlabs <ankush.patel@marlabs.com>
2019-02-20 12:50:17 +0530

    added https

--- api/views.py
+++ api/views.py
@@ -1272,9 +1272,9 @@ def home(request):
     host = request.get_host()
 
     APP_BASE_URL = ""
-    protocol = "http"
+    protocol = "https"
     if request.is_secure():
-        protocol = "http"
+        protocol = "https"
 
     SCORES_BASE_URL = "https://{}:8001/".format(settings.HDFS.get("host", "ec2-34-205-203-38.compute-1.amazonaws.com"))
     APP_BASE_URL = "{}://{}".format(protocol, host)
