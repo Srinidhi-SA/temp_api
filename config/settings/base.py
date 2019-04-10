f8b9e02f919328a8bb7699e560de4cde0976d52c
ankushmarlabs <ankush.patel@marlabs.com>
2019-04-10 12:27:31 +0530

    added traintest in brief info

--- config/settings/base.py
+++ config/settings/base.py
@@ -721,6 +721,7 @@ BRIEF_INFO_CONFIG = {
     'dataset': 'Dataset',
     'analysis list': 'Analysis List',
     'train_test_split': 'Train Test Split',
+    'trainAndtest': 'Train Test Split',
     'kFold': 'K Fold Validation',
     'algorithm name': 'Algorithm Name',
     'model': 'Model',
@@ -757,6 +758,7 @@ DATASET_ORDER = [
 THIRD_ORDER = [
     'analysis list',
     'train_test_split',
+    'trainAndtest',
     'kFold',
     'model',
     'algorithm name'
