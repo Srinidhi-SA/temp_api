5153b630ccdee0724d3169a60b85ca6b077abdd2
sanjeev-das <sanjeev.das@marlabs.com>
2019-09-09 13:12:29 +0530

    Added automl settings in MLsettings

--- config/settings/mlsettings/mlSettings.py
+++ config/settings/mlsettings/mlSettings.py
@@ -2,6 +2,7 @@ from pySparkMLClassificationParams import *
 from pySparkMLRegressionParams import *
 from sklearnMLClassificationParams import *
 from sklearnMLRegressionParams import *
+from autoMLSettings import *
 import copy
 
 ALGORITHMRANDOMSLUG = "f77631ce2ab24cf78c55bb6a5fce4db8"
@@ -332,7 +333,7 @@ else:
           },
           {
             "algorithmName": "Neural Network",
-            "selected": False,
+            "selected": True,
             "parameters": SKLEARN_ML_NEURAL_NETWORK_PARAMS,
             "algorithmSlug": ALGORITHMRANDOMSLUG+"mlp",
             "hyperParameterSetting":SKLEARN_HYPERPARAMETER_OBJECT_REGRESSION,
@@ -383,7 +384,7 @@ else:
             },
             {
                 "algorithmName": "Neural Network",
-                "selected": False,
+                "selected": True,
                 "parameters": SKLEARN_ML_NEURAL_NETWORK_PARAMS,
                 "algorithmSlug": ALGORITHMRANDOMSLUG + "mlp",
                 "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_CLASSIFICATION,
@@ -399,3 +400,103 @@ else:
             # }
         ]
     }
+
+#########################   CONFIG FOR AUTO ML   ############################
+
+AUTOML_ALGORITHM_LIST_REGRESSION={
+    "ALGORITHM_SETTING":[
+      {
+        "algorithmName": "Linear Regression",
+        "selected": True,
+        "parameters": AUTOML_SKLEARN_ML_LINEAR_REGRESSION_PARAMS,
+        "algorithmSlug": ALGORITHMRANDOMSLUG+"linr",
+        "hyperParameterSetting":SKLEARN_HYPERPARAMETER_OBJECT_REGRESSION,
+        "description":"A statistical method to predict the likely outcome of any quantitative attribute. It is invariably used for estimating values of any numeric variables like sales, number of products, etc."
+      },
+      {
+        "algorithmName": "Gradient Boosted Tree Regression",
+        "selected": True,
+        "parameters": AUTOML_SKLEARN_ML_GBT_REGRESSION_PARAMS,
+        "algorithmSlug": ALGORITHMRANDOMSLUG+"gbtr",
+        "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_REGRESSION,
+        "description":"A machine learning technique that produces an ensemble of multiple decision tree models to predict numeric variables. It is highly preferred to leverage computational power to build scalable and accurate models."
+
+      },
+      {
+        "algorithmName": "Decision Tree Regression",
+        "selected": True,
+        "parameters": AUTOML_SKLEARN_ML_DTREE_REGRESSION_PARAMS,
+        "algorithmSlug": ALGORITHMRANDOMSLUG+"dtreer",
+        "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_REGRESSION,
+        "description":"A machine learning technique that produces an ensemble of multiple decision tree models to predict numeric variables. It is highly preferred to leverage computational power to build scalable and accurate models."
+      },
+      {
+        "algorithmName": "Random Forest Regression",
+        "selected": True,
+        "parameters": AUTOML_SKLEARN_ML_RF_REGRESSION_PARAMS,
+        "algorithmSlug": ALGORITHMRANDOMSLUG+"rfr",
+        "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_REGRESSION,
+        "description":"A meta estimator that uses averaging predictive power of a number of decision tree classification models. This is very effective in predicting the expected values of numeric variables and also to control overfitting."
+      },
+      {
+        "algorithmName": "Neural Network",
+        "selected": False,
+        "parameters": SKLEARN_ML_NEURAL_NETWORK_PARAMS,
+        "algorithmSlug": ALGORITHMRANDOMSLUG+"mlp",
+        "hyperParameterSetting":SKLEARN_HYPERPARAMETER_OBJECT_REGRESSION,
+        "description":"A statistical method to predict the likely outcome of any quantitative attribute. It is invariably used for estimating values of any numeric variables like sales, number of products, etc."
+      },
+    ]
+}
+AUTOML_ALGORITHM_LIST_CLASSIFICATION = {
+    "ALGORITHM_SETTING": [
+        {
+            "algorithmName": "Logistic Regression",
+            "selected": True,
+            "parameters": AUTOML_SKLEARN_ML_LOGISTIC_REGRESSION_PARAMS,
+            "algorithmSlug": ALGORITHMRANDOMSLUG + "lr",
+            "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_CLASSIFICATION,
+            "description":"A statistical method to predict the likely outcome of any qualitative attribute. It is invariably used for predicting binary outcomes (such as Yes or No)."
+        },
+        {
+            "algorithmName": "Random Forest",
+            "selected": True,
+            "parameters": AUTOML_SKLEANR_ML_RF_CLASSIFICATION_PARAMS,
+            "algorithmSlug": ALGORITHMRANDOMSLUG + "rf",
+            "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_CLASSIFICATION,
+            "description":"""A meta estimator that uses averaging predictive power of a number of decision tree
+            classification models. This is very effective in predicting the likelihood in multi-class
+            classifications and also to control overfitting."""
+        },
+        {
+            "algorithmName": "XGBoost",
+            "selected": True,
+            "parameters": AUTOML_SKLEARN_ML_XGBOOST_CLASSIFICATION_PARAMS,
+            "algorithmSlug": ALGORITHMRANDOMSLUG + "xgb",
+            "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_CLASSIFICATION,
+            "description":"""A machine learning technique that produces an ensemble of multiple decision tree
+            models to predict categorical variables. It is highly preferred to leverage
+            computational power to build scalable and accurate models."""
+        },
+        {
+            "algorithmName": "naive bayes",
+            "selected": True,
+            "parameters": AUTOML_SKLEARN_ML_NAIVE_BAYES_PARAMS,
+            "algorithmSlug": ALGORITHMRANDOMSLUG + "nb",
+            "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_CLASSIFICATION,
+            "description": """The multinomial Naive Bayes classifier is suitable for classification with discrete
+            features (e.g., word counts for text classification).
+            The multinomial distribution normally requires integer feature counts.
+            However, in practice, fractional counts such as tf-idf may also work."""
+        },
+        {
+            "algorithmName": "Neural Network",
+            "selected": False,
+            "parameters": SKLEARN_ML_NEURAL_NETWORK_PARAMS,
+            "algorithmSlug": ALGORITHMRANDOMSLUG + "mlp",
+            "hyperParameterSetting": SKLEARN_HYPERPARAMETER_OBJECT_CLASSIFICATION,
+            "description":"This model optimizes the log-loss function using LBFGS or stochastic gradient descent."
+        },
+
+    ]
+}
