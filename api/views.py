37d1fd5f98274909fb5e584a8db087b4f321e077
Ajay <ajay.vijayakumar@marlabs.com>
2019-10-16 21:19:59 +0530

    API-Trainer Unique name

--- api/views.py
+++ api/views.py
@@ -138,7 +138,6 @@ class SignalView(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         data = request.data
         data = convert_to_string(data)
-        print 'upodate signal data', data
         if 'name' in data:
             should_proceed = name_check(data['name'])
             if should_proceed < 0:
@@ -224,6 +223,12 @@ class TrainerView(viewsets.ModelViewSet):
         data = convert_to_string(data)
 
         if 'name' in data:
+            trainername_list = []
+            trainer_obj = Trainer.objects.filter()
+            for index, i in enumerate(trainer_obj):
+                trainername_list.append(i.name)
+            if data['name'] in trainername_list:
+                return creation_failed_exception("Name already exists!.")
             should_proceed = name_check(data['name'])
             if should_proceed < 0:
                 if should_proceed == -1:
