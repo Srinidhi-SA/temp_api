cebde69fd1ef8aac2f058b20a6a4608475f20ea7
sanjeev-das <sanjeev.das@marlabs.com>
2019-10-28 13:26:41 +0530

    Added timeout kill method

--- static/react/src/app/actions/dataActions.js
+++ static/react/src/app/actions/dataActions.js
@@ -209,6 +209,18 @@ columns
         if (dataPreview.message && dataPreview.message !== null && dataPreview.message.length > 0) {
             dispatch(openAppsLoaderValue(dataPreview.message[0].stageCompletionPercentage, dataPreview.message[0].shortExplanation));
         }
+        setTimeout(function() {
+           if(dataPreview.message[0].globalCompletionPercentage<=0){
+               return fetch(API + '/api/kill_timeout_job_from_ui/?slug='+slug, {
+                   method: 'get',
+                   headers: getHeader(getUserDetailsOrRestart.get().userToken)
+               }).then(response => Promise.all([response, response.json()])).catch(function(error){
+               bootbox.alert("Unable to connect to server. Check your connection please try again.")
+                 });
+                }
+
+
+           },420000);
         return {
             type: "SELECTED_DATASET",
             dataset,
