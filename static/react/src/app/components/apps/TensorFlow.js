9df651f237bc848172e5483969bc0ae8a6c4a759
SuryatejaS <sunkara.surya@marlabs.com>
2019-12-04 11:46:19 +0530

    tf:top level data

--- static/react/src/app/components/apps/TensorFlow.js
+++ static/react/src/app/components/apps/TensorFlow.js
@@ -15,10 +15,43 @@ export class TensorFlow extends React.Component {
         super(props);
     }
 
+    getOptions(item) {
+      var arr = item.defaultValue.map(j=>j.displayName);
+      var options = arr.map(k => {
+          return <option value={k} > {k}</option>
+      })
+      return <select className="form-control"> {options} </select>
+    }
+    
+
     render() {
+     var algorithmData=this.props.manualAlgorithmData[5].parameters
+     var rendercontent = algorithmData.map((item,index)=>{
+           if(item.paramType=="list"){
+              return (
+                <div class="form-group">
+                <label class="col-md-4 control-label read">{item.displayName}</label>
+                 <div className="col-md-6 for_multiselect">
+                  {this.getOptions(item)}
+                   </div>
+                 <div class="clearfix"></div>
+                </div>
+              )
+           }
+           else
+            return (
+                <div class="form-group">
+                <label class="col-md-4 control-label read">{item.displayName}</label>
+                 <div className="col-md-2">
+                   <input type="number" className="form-control" onKeyDown={ (evt) => evt.key === 'e' && evt.preventDefault() } defaultValue={item.defaultValue} value={item.defaultValue} />
+                   </div>
+                 <div class="clearfix"></div>
+                </div>                 
+              )
+       })
         return (
                 <div class="col-md-6">
-                    Tensor Component
+                    {rendercontent}
                 </div>
         );
 
