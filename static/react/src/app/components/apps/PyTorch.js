9e68c07a27ae1bb2e25fa0e4979994d2ac26465f
VidyaR13 <vidya.ramaiah@marlabs.com>
2019-12-11 18:36:42 +0530

    Added pytorch component and changing branch

--- static/react/src/app/components/apps/PyTorch.js
+++ static/react/src/app/components/apps/PyTorch.js
@@ -4,10 +4,28 @@ import {Redirect} from "react-router";
 import store from "../../store";
 import {statusMessages} from  "../../helpers/helper"
 
+@connect((store)=>{
+    return{
+        algorithmData:store.apps.regression_algorithm_data,
+        manualAlgorithmData:store.apps.regression_algorithm_data_manual,
+    }
+})
+
 export class PyTorch extends React.Component {
-    render() {    
+    constructor(props){
+        super(props);
+    }
+    render() {
+        let renderPyTorchContent = "Pytorch Content"
+        this.props.algorithmData
         return (        
-          <div>PyTorch</div>
+            <div className="col-md-12">
+                <div className="row mb-20">
+                    <div class="form-group">
+                        {renderPyTorchContent}
+                    </div>
+                </div>
+          </div>
         );
 
     }
