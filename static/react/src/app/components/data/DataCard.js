70b40fdeb3e0a88fa83b1ba1597a65cfbd177d69
rajshreegupta21 <rajshree.gupta@marlabs.com>
2019-12-13 18:59:18 +0530

    edit button UI change for score signal and data

--- static/react/src/app/components/data/DataCard.js
+++ static/react/src/app/components/data/DataCard.js
@@ -193,7 +193,7 @@ export class DataCard extends React.Component {
 								? "Stop"
 								: "Delete"}</a>
                                 </span>: ""}
-                                {data.status == "SUCCESS"? <span  className="shareButton"onClick={this.openShareModal.bind(this,data.name,data.slug,"Data")}>
+                                {data.status == "SUCCESS"? <span  className="shareButtonCenter"onClick={this.openShareModal.bind(this,data.name,data.slug,"Data")}>
 								<a className="dropdown-item btn-primary" href="#shareCard" data-toggle="modal">
 								<i className="fa fa-share-alt"></i>&nbsp;&nbsp;{"Share"}</a>
 								</span>: ""}
