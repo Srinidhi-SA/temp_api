215f400c0de544a4723a0f7696e18910cf5ef35f
rajshreegupta21 <rajshree.gupta@marlabs.com>
2019-12-24 15:57:00 +0530

    tensor flow add layer UI change

--- static/react/src/app/components/apps/TensorFlow.js
+++ static/react/src/app/components/apps/TensorFlow.js
@@ -69,6 +69,9 @@ export class TensorFlow extends React.Component {
       bootbox.alert(statusMessages("warning", "Please select an alternate layer.", "small_mascot"));
       return false
       }
+      else{
+        this.addLayer(slectedLayer);
+      }
   }
 
    parameterValidate=()=>{
