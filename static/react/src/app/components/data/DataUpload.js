ab7205f4ffd0f027f321fe2be0607ee724f91bd2
rajshreegupta21 <rajshree.gupta@marlabs.com>
2020-01-07 17:38:15 +0530

    adding unique is for data tab

--- static/react/src/app/components/data/DataUpload.js
+++ static/react/src/app/components/data/DataUpload.js
@@ -75,8 +75,8 @@ export class DataUpload extends React.Component {
               <DataSourceList/>
             </Modal.Body>
             <Modal.Footer>
-              <Button onClick={this.closePopup.bind(this)}>Close</Button>
-              <Button bsStyle="primary" onClick={this.uploadData.bind(this)}>Load Data</Button>
+              <Button id="dataCloseBtn" onClick={this.closePopup.bind(this)}>Close</Button>
+              <Button id="loadDataBtn" bsStyle="primary" onClick={this.uploadData.bind(this)}>Load Data</Button>
             </Modal.Footer>
           </Modal>
         </div>
