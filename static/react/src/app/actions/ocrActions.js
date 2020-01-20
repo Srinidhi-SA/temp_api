67f6d7786669fea3e060796b835e907013e09746
rajshreegupta21 <rajshree.gupta@marlabs.com>
2020-01-20 14:11:44 +0530

    UI OCR files added

---
+++ static/react/src/app/actions/ocrActions.js
@@ -0,0 +1,1 @@
+//all the ocr related actions..
---
+++ static/react/src/app/components/apps/Ocr.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export class Ocr extends React.Component {
+
+ componentDidMount() {
+    }
+  
+    render() {
+      
+      return (
+		        <div className="side-body">
+              <button style={{margin:20}}>Upload</button>
+            </div>
+        )
+
+
+    }
+
+
+
+}
