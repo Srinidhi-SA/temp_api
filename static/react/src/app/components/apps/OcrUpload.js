b420c3e48c84a5ca8768034482589a3b7691899d
rajshreegupta21 <rajshree.gupta@marlabs.com>
2020-01-22 17:00:04 +0530

    UI changes for OCR file upload

--- static/react/src/app/components/apps/OcrUpload.js
+++ static/react/src/app/components/apps/OcrUpload.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {connect} from "react-redux";
-
+import { connect } from "react-redux";
+import { STATIC_URL } from "../../helpers/env.js";
+import { Scrollbars } from 'react-custom-scrollbars';
 @connect((store) => {
   return {
     OcrfileUpload: store.ocr.OcrfileUpload,
@@ -16,51 +17,49 @@ export class OcrUpload extends React.Component {
     }
   }
 
-onChangeHandler=event=>{
+  onChangeHandler = event => {
     console.log(event.target.files);
     this.setState({
       selectedFiles: Object.values(event.target.files),
-     })
-}
+    })
+  }
 
-removeFile(item){
-  this.setState({
-    selectedFiles: Object.values(this.state.selectedFiles).filter(i=>i.name!=item),
-   })
-  
-}
+  removeFile(item) {
+    this.setState({
+      selectedFiles: Object.values(this.state.selectedFiles).filter(i => i.name != item),
+    })
 
-render() {
-    var fileNames= this.state.selectedFiles!=""?Object.values(this.state.selectedFiles).map(i=>i.name).map((item,index)=>(
-      <ul className="list-unstyled bullets_primary">
-        <li>{item}
-        <span style={{marginLeft:"10px"}}onClick={this.removeFile.bind(this,item)}>
-        <i class="fa fa-times" aria-hidden="true"></i>
+  }
+
+  render() {
+    var fileNames = this.state.selectedFiles != "" ? Object.values(this.state.selectedFiles).map(i => i.name).map((item, index) => (
+      <li>{item}
+        <span style={{ marginLeft: "10px" }} onClick={this.removeFile.bind(this, item)}>
+          <i class="fa fa-times" aria-hidden="true" style={{ color: '#555' }}></i>
         </span>
-        </li>
-      </ul>
+      </li>
     ))
-    :""
+      : ""
 
-    var content =(
-                <div>
-                  File Upload
-                  <input type="file" multiple onChange={this.onChangeHandler}/>
-                  <div className=" ">
-                    {/* <Dropzone id={1} onDrop={this.onChangeHandler}  multiple>
-                      <p>Please drag and drop your file here or browse.</p>
-                    </Dropzone> */}
-                    <aside>
-                      {fileNames}
-                    </aside>
-                  </div>
-                 </div>
-    )
     return (
-        <div>
-        {content}
+      <div className="row">
+        <div className="col-md-5 ocrUploadHeight">
+          <div className="dropzoneOcr">
+            <input className="ocrUpload" type="file" multiple onChange={this.onChangeHandler} />
+            <img style={{ height: 64, width: 64, opacity: 0.4, zIndex: 0, cursor: 'pointer' }} src={STATIC_URL + "assets/images/ocrUpload.svg"} />
+            <span>Upload files</span>
+          </div>
         </div>
+
+        <div className="col-md-7 ocrUploadHeight">
+          <Scrollbars>
+            <ul className="list-unstyled bullets_primary ocrUploadHeight">
+              {fileNames}
+            </ul>
+          </Scrollbars>
+        </div>
+      </div>
     )
-}
+  }
 
 }
