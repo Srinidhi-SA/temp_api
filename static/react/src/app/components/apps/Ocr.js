1d3e6203612fb466fd9d8f9cd449e8c6affdeb56
SuryatejaS <sunkara.surya@marlabs.com>
2020-01-22 18:07:37 +0530

    components isolated

--- static/react/src/app/components/apps/Ocr.js
+++ static/react/src/app/components/apps/Ocr.js
@@ -1,55 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Modal, Button, } from "react-bootstrap";
-import store from "../../store";
-import { open, close, fileUpload, dataUpload } from "../../actions/dataUploadActions";
-import { updateSelectedDataSrc } from "../../actions/dataSourceListActions";
 import { OcrUpload } from "../apps/OcrUpload";
-
 @connect((store) => {
-  return { login_response: store.login.login_response, showModal: store.dataUpload.dataUploadShowModal, fileDataUpload: store.dataUpload.fileUpload };
+  return { 
+    login_response: store.login.login_response 
+    };
 })
 
 export class Ocr extends React.Component {
   constructor(props) {
     super(props);
-    this.props.dispatch(close());
-    this.onDrop = this.onDrop.bind(this);
+    
   }
-  openPopup() {
-    this.props.dispatch(open());
-  }
-  closePopup() {
-    this.props.dispatch(close())
-    this.props.dispatch(updateSelectedDataSrc("fileUpload"))
-  }
-  onDrop(files) {
-    this.props.dispatch(fileUpload(files[0]))
-  }
-  uploadData() {
-    this.props.dispatch(dataUpload());
-  }
+  
   render() {
     return (
       <div className="side-body">
         <div className="main-content">
-          <div class="row">
-            <Button style={{ marginLeft: "35px" }} bsStyle="primary" onClick={this.openPopup.bind(this)}>Upload</Button>
-            <div id="uploadData" role="dialog" className="modal fade modal-colored-header">
-              <Modal show={store.getState().dataUpload.dataUploadShowModal} onHide={this.closePopup.bind(this)} dialogClassName="modal-colored-header uploadData">
-                <Modal.Header closeButton>
-                  <h3 className="modal-title">Upload Data</h3>
-                </Modal.Header>
-                <Modal.Body>
-                  <OcrUpload />
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button id="dataCloseBtn" bsStyle="primary" onClick={this.uploadData.bind(this)}>Upload Data</Button>
-                  <Button id="loadDataBtn" bsStyle="primary" disabled>Proceed</Button>
-                </Modal.Footer>
-              </Modal>
-            </div>
-          </div>
+           <OcrUpload />     
         </div>
       </div>
     );
--- static/react/src/app/components/apps/OcrUpload.js
+++ static/react/src/app/components/apps/OcrUpload.js
@@ -1,25 +1,39 @@
 import React from "react";
 import {connect} from "react-redux";
-import {Button} from "react-bootstrap";
+import { Modal, Button, } from "react-bootstrap";
 import {getUserDetailsOrRestart} from "../../helpers/helper"
 import { STATIC_URL } from "../../helpers/env.js";
 import { Scrollbars } from 'react-custom-scrollbars';
+import store from "../../store";
+import { open, close, fileUpload, dataUpload } from "../../actions/dataUploadActions";
+import { updateSelectedDataSrc } from "../../actions/dataSourceListActions";
 @connect((store) => {
   return {
     OcrfileUpload: store.ocr.OcrfileUpload,
-
+    login_response: store.login.login_response,
+    showModal: store.dataUpload.dataUploadShowModal,
+    // fileDataUpload: store.dataUpload.fileUpload
   };
 })
 
 export class OcrUpload extends React.Component {
   constructor(props) {
     super(props);
+    this.props.dispatch(close());
     this.state = {
       selectedFiles: ""
     }
   }
 
-  onChangeHandler=event=>{
+  openPopup() {
+    this.props.dispatch(open());
+  }
+  closePopup() {
+    this.props.dispatch(close())
+    this.props.dispatch(updateSelectedDataSrc("fileUpload"))
+  }
+
+  onDrop=event=>{
     console.log(event.target.files);
     this.setState({
       selectedFiles: Object.values(event.target.files),
@@ -61,25 +75,40 @@ export class OcrUpload extends React.Component {
     ))
       : ""
 
-    return (
-      <div className="row">
-        <div className="col-md-5 ocrUploadHeight">
-          <div className="dropzoneOcr">
-            <input className="ocrUpload" type="file" multiple onChange={this.onChangeHandler} />
-            <img style={{ height: 64, width: 64, opacity: 0.4, zIndex: 0, cursor: 'pointer' }} src={STATIC_URL + "assets/images/ocrUpload.svg"} />
-            <span>Upload files</span>
-          </div>
-        </div>
-
-        <div className="col-md-7 ocrUploadHeight">
-          <Scrollbars>
-            <ul className="list-unstyled bullets_primary ocrUploadHeight">
-              {fileNames}
-            </ul>
-          </Scrollbars>
-        </div>
+  return (
+    <div class="row">
+     <Button style={{ marginLeft: "35px" }} bsStyle="primary" onClick={this.openPopup.bind(this)}>Upload</Button>
+      <div id="uploadData" role="dialog" className="modal fade modal-colored-header">
+        <Modal show={store.getState().dataUpload.dataUploadShowModal} onHide={this.closePopup.bind(this)} dialogClassName="modal-colored-header">
+          <Modal.Header closeButton>
+            <h3 className="modal-title">Upload Data</h3>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="row">
+              <div className="col-md-5 ocrUploadHeight">
+                <div className="dropzoneOcr">
+                  <input className="ocrUpload" type="file" multiple onChange={this.onDrop} />
+                  <img style={{ height: 64, width: 64, opacity: 0.4, zIndex: 0, cursor: 'pointer' }} src={STATIC_URL + "assets/images/ocrUpload.svg"} />
+                  <span>Upload files</span>
+                </div>
+              </div>
+              <div className="col-md-7 ocrUploadHeight">
+                <Scrollbars>
+                <ul className="list-unstyled bullets_primary ocrUploadHeight">
+                  {fileNames}
+                </ul>
+                </Scrollbars>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+                <Button id="dataCloseBtn" bsStyle="primary" onClick={this.handleSubmit.bind(this,this.state.selectedFiles)}>Upload Data</Button>
+                <Button id="loadDataBtn" bsStyle="primary" disabled>Proceed</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-    )
+    </div>
+  )
   }
 
 }
