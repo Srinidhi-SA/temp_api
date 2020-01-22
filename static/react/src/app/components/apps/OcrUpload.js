87f9c51797134273f87cb07e3f900ad5a2ca0d2c
SuryatejaS <sunkara.surya@marlabs.com>
2020-01-22 16:13:14 +0530

    api func modif

--- static/react/src/app/components/apps/OcrUpload.js
+++ static/react/src/app/components/apps/OcrUpload.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
-
+import { Button} from "react-bootstrap";
+import {getUserDetailsOrRestart} from "../../helpers/helper"
 @connect((store) => {
   return {
     OcrfileUpload: store.ocr.OcrfileUpload,
@@ -16,21 +17,40 @@ export class OcrUpload extends React.Component {
     }
   }
 
-onChangeHandler=event=>{
+  onChangeHandler=event=>{
     console.log(event.target.files);
     this.setState({
       selectedFiles: Object.values(event.target.files),
      })
-}
+  }
 
-removeFile(item){
+  removeFile(item){
   this.setState({
     selectedFiles: Object.values(this.state.selectedFiles).filter(i=>i.name!=item),
    })
-  
-}
+  }
 
-render() {
+  getHeader = token => {
+    return {
+      Authorization: token
+    };
+  };
+
+  handleSubmit(acceptedFiles ){
+    var data = new FormData();
+    console.log(this.state.selectedFiles);
+    for (var x = 0; x < acceptedFiles.length; x++) {
+      data.append("file", acceptedFiles[x]);
+    }
+    return fetch("https://madvisor-dev.marlabsai.com/ocr/ocrimage/", {
+      method: "POST",
+      headers: this.getHeader(getUserDetailsOrRestart.get().userToken),
+      body: data
+    });
+  };
+
+
+  render(){
     var fileNames= this.state.selectedFiles!=""?Object.values(this.state.selectedFiles).map(i=>i.name).map((item,index)=>(
       <ul className="list-unstyled bullets_primary">
         <li>{item}
@@ -53,14 +73,15 @@ render() {
                     <aside>
                       {fileNames}
                     </aside>
+                    <Button bsStyle="primary" onClick={this.handleSubmit.bind(this,this.state.selectedFiles)}> Upload</Button>
                   </div>
                  </div>
     )
-    return (
+   return (
         <div>
         {content}
         </div>
-    )
-}
+   )
+ }
 
 }
