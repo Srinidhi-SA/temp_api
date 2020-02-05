9b60a42ce03a69310d038104a9daeab1a7a7283a
sabya93 <sabyasachi.das@marlabs.com>
2020-02-05 12:14:25 +0530

    Added max file upload validator

--- ocr/validators.py
+++ ocr/validators.py
@@ -12,3 +12,9 @@ def max_file_size(value):
     limit = 50 * 1024 * 1024
     if value.size > limit:
         raise ValidationError('File too large. Size should not exceed 50 MB.')
+
+
+def max_num_files(value):
+    """ METHOD : To Validate max number of file upload for OCRImage model FileField. """
+    if len(value) > 100:
+        raise ValidationError("Can't upload more than 100 files")
