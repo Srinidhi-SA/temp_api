9081a4d89859fde7b100aecb5aedd7395e1f43c2
rajshreegupta21 <rajshree.gupta@marlabs.com>
2020-02-13 12:48:45 +0530

    ocr UI changes for placing image on the canvas

--- static/react/src/app/components/apps/OcrUpload.js
+++ static/react/src/app/components/apps/OcrUpload.js
@@ -6,7 +6,7 @@ import { STATIC_URL } from "../../helpers/env.js";
 import { Scrollbars } from 'react-custom-scrollbars';
 import store from "../../store";
 import { open, close } from "../../actions/dataUploadActions";
-import {getOcrUploadedFiles} from '../../actions/ocrActions'
+import { getOcrUploadedFiles } from '../../actions/ocrActions'
 @connect((store) => {
   return {
     OcrfileUpload: store.ocr.OcrfileUpload,
@@ -80,7 +80,9 @@ export class OcrUpload extends React.Component {
     var data = new FormData();
     for (var x = 0; x < acceptedFiles.length; x++) {
       data.append("imagefile", acceptedFiles[x]);
+      data.append("dataSourceType", "fileUpload");
     }
+
     return fetch("https://madvisor-dev.marlabsai.com/ocr/ocrimage/", {
       method: "POST",
       headers: this.getHeader(getUserDetailsOrRestart.get().userToken),
@@ -105,11 +107,11 @@ export class OcrUpload extends React.Component {
         </span>
       </li>
     ))
-      : <div>No files chosen.<br/>Please select file to proceed.</div>
+      : <div>No files chosen.<br />Please select file to proceed.</div>
 
     return (
       <div>
-        <Button bsStyle="primary" onClick={this.openPopup.bind(this)} style={{marginBottom:20}}><i class="fa fa-upload"></i> Upload</Button>
+        <Button bsStyle="primary" onClick={this.openPopup.bind(this)} style={{ marginBottom: 20 }}><i class="fa fa-upload"></i> Upload</Button>
         <div id="uploadData" role="dialog" className="modal fade modal-colored-header">
           <Modal show={store.getState().dataUpload.dataUploadShowModal} onHide={this.closePopup.bind(this)} dialogClassName="modal-colored-header">
             <Modal.Header closeButton>
