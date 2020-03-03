5816791ef5485aebc58e3c813e58096401c3aeb6
VidyaR13 <vidya.ramaiah@marlabs.com>
2020-03-03 17:07:32 +0530

    Added tabs, pagination, search functionalities and changed fetchAllUsers call

--- static/react/src/app/components/apps/Ocr/OcrUserTable.js
+++ static/react/src/app/components/apps/Ocr/OcrUserTable.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Modal, Button} from "react-bootstrap";
+import { Pagination} from "react-bootstrap";
 import store from "../../../store";
-import {fetchAllOcrUsersAction, deleteOcrUserAction, saveSelectedOcrUserList, openEditUserModalAction, getReviewersListAction, enableEditingUserAction, activateOcrUserAction, deActivateOcrUserAction, openAddUserPopup, setUserTableLoaderFlag} from "../../../actions/ocrActions";
+import {fetchAllOcrUsersAction, deleteOcrUserAction, saveSelectedOcrUserList, openEditUserModalAction, getReviewersListAction, activateOcrUserAction, deActivateOcrUserAction, openAddUserPopup, setUserTableLoaderFlag, storeSelectedTabId, fetchOcrListByReviewerType, clearUserSearchElementAction, saveUserSearchElementAction, selectAllOcrUsers, saveOcrUserPageNumAction} from "../../../actions/ocrActions";
 import { statusMessages } from "../../../helpers/helper";
 import { Checkbox } from "primereact/checkbox";
 import { OcrAddUser } from "./OcrAddUser";
@@ -11,16 +11,14 @@ import { STATIC_URL } from "../../../helpers/env.js";
 
 @connect((store) => {
   return {
-    addUserPopupFlag : store.ocr.addUserPopupFlag,
-    createUserFlag : store.ocr.createUserFlag,
-    newUserDetails : store.ocr.newUserDetails,
     allOcrUsers : store.ocr.allOcrUsers,
-    newUserProfileDetails : store.ocr.newUserProfileDetails,
-    ocrUserProfileFlag : store.ocr.ocrUserProfileFlag,
     selectedOcrUsers : store.ocr.selectedOcrUsers,
-    selUserSlug : store.ocr.selUserSlug,
     selUserDetails : store.ocr.selUserDetails,
     userTableLoaderFlag : store.ocr.userTableLoaderFlag,
+    ocrReviwersList : store.ocr.ocrReviwersList,
+    selectedTabId : store.ocr.selectedTabId,
+    isAllCheckedFlag : store.ocr.isAllCheckedFlag,
+    ocrUserPageNum : store.ocr.ocrUserPageNum,
   };
 })
 
@@ -31,7 +29,7 @@ export class OcrUserTable extends React.Component{
 
     componentWillMount(){
         this.props.dispatch(setUserTableLoaderFlag(true));
-        this.props.dispatch(fetchAllOcrUsersAction());
+        this.props.selectedTabId === "none"?this.props.dispatch(fetchAllOcrUsersAction()):this.props.dispatch(fetchOcrListByReviewerType(parseFloat(this.props.selectedTabId)));
         this.props.dispatch(getReviewersListAction());
     }
 
@@ -45,6 +43,20 @@ export class OcrUserTable extends React.Component{
             curSelUsers.push(e.target.value)
         else
             curSelUsers.splice(curSelUsers.indexOf(e.value), 1)
+        this.props.dispatch(saveSelectedOcrUserList(curSelUsers));
+        curSelUsers.length === 10 ? this.props.dispatch(selectAllOcrUsers(true)): this.props.dispatch(selectAllOcrUsers(false))
+    }
+    selectAllUsers(e){
+        this.props.dispatch(selectAllOcrUsers(!this.props.isAllCheckedFlag));
+        let curSelUsers = [...this.props.selectedOcrUsers];
+        if(!this.props.isAllCheckedFlag){
+            this.props.allOcrUsers.data.map((item) => {
+                if(item.ocr_user)
+                    curSelUsers.push(item.username)
+            });
+        }else{
+            curSelUsers = [];
+        }
         this.props.dispatch(saveSelectedOcrUserList(curSelUsers));
     }
     selectActiontype(e){
@@ -62,9 +74,13 @@ export class OcrUserTable extends React.Component{
                     this.props.dispatch(deActivateOcrUserAction(this.props.selectedOcrUsers));
                     break;
                 case "edit":
-                        let selUserDetails = ""
-                        selUserDetails = this.props.allOcrUsers.data.filter(i=>i.username === this.props.selectedOcrUsers[0])[0]
-                        this.props.dispatch(openEditUserModalAction(true,this.props.selectedOcrUsers[0],selUserDetails));
+                        if(this.props.selectedOcrUsers.length>1)
+                            bootbox.alert(statusMessages("warning", "Please select only one user", "small_mascot"))
+                        else{
+                            let selUserDetails = ""
+                            selUserDetails = this.props.allOcrUsers.data.filter(i=>i.username === this.props.selectedOcrUsers[0])[0]
+                            this.props.dispatch(openEditUserModalAction(true,this.props.selectedOcrUsers[0],selUserDetails));
+                        }
                     break;
                 default:
                     break;
@@ -74,7 +90,31 @@ export class OcrUserTable extends React.Component{
     openEditUserModal(userData){
         this.props.dispatch(openEditUserModalAction(true,userData.ocr_profile.slug,userData));
     }
+    filterByReviewerType(e){
+        this.props.dispatch(storeSelectedTabId(e.target.id));
+        this.props.dispatch(setUserTableLoaderFlag(true));
+        if(e.target.id === "none"){
+            this.props.dispatch(fetchAllOcrUsersAction(store.getState().ocr.ocrUserPageNum));
+        }else{
+            this.props.dispatch(fetchOcrListByReviewerType(parseFloat(e.target.id),store.getState().ocr.ocrUserPageNum));
+        }        
+    }
+    handlePagination(pageNo){
+        this.props.dispatch(saveOcrUserPageNumAction(pageNo));
+        this.props.selectedTabId != "none"?
+            this.props.dispatch(fetchOcrListByReviewerType(parseFloat(this.props.selectedTabId),store.getState().ocr.ocrUserPageNum))
+            : this.props.dispatch(fetchAllOcrUsersAction(store.getState().ocr.ocrUserPageNum))
+    }
+    handleSearchElement(e){
+        this.props.dispatch(saveUserSearchElementAction(e.target.value));
+        this.props.selectedTabId === "none"?this.props.dispatch(fetchAllOcrUsersAction(store.getState().ocr.ocrUserPageNum)):this.props.dispatch(fetchOcrListByReviewerType(parseFloat(this.props.selectedTabId),store.getState().ocr.ocrUserPageNum));
+    }
+    clearSearchVal(){
+        document.getElementById("searchOcrUser").value=""
+        this.props.dispatch(clearUserSearchElementAction());
+    }
     render(){
+        let paginationTag = null;
         let manageUsersTable = ""
         if(this.props.userTableLoaderFlag){
             manageUsersTable = <div style={{ height: "150px", background: "#ffffff", position: 'relative' }}>
@@ -84,11 +124,20 @@ export class OcrUserTable extends React.Component{
             manageUsersTable = <div className="noOcrUsers">
                 <span>No Users Found<br/>Please Click on add icon to add users</span>
             </div>
+        }else if(this.props.allOcrUsers.data.length <= 0){
+            manageUsersTable = <div className="noOcrUsers">
+                <span><br/>No Results Found<br/></span>
+            </div>
         }else{
+            const pages = store.getState().ocr.allOcrUsers.total_number_of_pages;
+            const current_page = store.getState().ocr.allOcrUsers.current_page;
+            if(pages > 1){
+                paginationTag = <Pagination  ellipsis bsSize="medium" maxButtons={10} onSelect={this.handlePagination.bind(this)} first last next prev boundaryLinks items={pages} activePage={current_page}/>
+            }
             manageUsersTable = 
                 <table className = "table manageUserTable">
                     <thead><tr>
-                        <th><Checkbox/></th>
+                        <th><Checkbox id="selectAll" value={this.props.allOcrUsers.data} onChange={this.selectAllUsers.bind(this)} checked={this.props.isAllCheckedFlag}/></th>
                         <th>FIRST NAME</th><th>LAST NAME</th>
                         <th>EMAIL</th><th>ROLES</th>
                         <th>DATE JOINED</th><th>LAST LOGIN</th>
@@ -115,25 +164,26 @@ export class OcrUserTable extends React.Component{
                     </tbody>
                 </table>
         }
+        let tabOptions=[];
+        tabOptions.push(<li className ="active"><a data-toggle="tab" id="none" name="none">All</a></li>);
+        for(var i=0; i<this.props.ocrReviwersList.length; i++){
+            tabOptions.push(<li><a data-toggle="tab" id={this.props.ocrReviwersList[i].id} name={this.props.ocrReviwersList[i].type}>{this.props.ocrReviwersList[i].type}</a></li>);
+        }
         return(
             <div>
-                <div className="col-md-12">
+                <div>
                         <h4>Manage users</h4>
                     </div>
                 <div className="row userActions">
                     <div className="col-md-8">
-                        <ul class="nav nav-tabs">
-                            <li class="active"><a href="javascript:;">All</a></li>
-                            <li><a href="javascript:;">Admin</a></li>
-                            <li><a href="javascript:;">Superuser</a></li>
-                            <li><a href="javascript:;">Reviewer L1</a></li>
-                            <li><a href="javascript:;">Reviewer L2</a></li>
+                        <ul className ="nav nav-tabs" onClick={this.filterByReviewerType.bind(this)} style={{cursor: "default"}}>
+                            {tabOptions}
                         </ul>
                     </div>
                     <div className="col-md-1">
                         <div className="btn-group ocrAddUser">
                             <a className="btn btn-info" onClick={this.openAddUserPopup.bind(this)}>
-                                <i class="fa fa-user-plus fa-lg">
+                                <i className ="fa fa-user-plus fa-lg">
                                     <OcrAddUser/>
                                 </i>
                             </a>
@@ -141,21 +191,27 @@ export class OcrUserTable extends React.Component{
                     </div>
                     <div className="col-md-3">
                         <div className="btn-group actionOcrUser">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">ACTION <span class="caret"></span></button>
-                            <ul role="menu" class="dropdown-menu dropdown-menu-right">
-                                <li><a name="actionType" id="activate" onClick={this.selectActiontype.bind(this)}><i class="fa fa-plus-circle text-primary"></i> Activate</a></li>
-                                <li><a name="actionType" id="deactivate" onClick={this.selectActiontype.bind(this)}><i class="fa fa-minus-circle text-warning"></i> Deactivate</a></li>
-                                <li><a name="actionType" id="delete" onClick={this.selectActiontype.bind(this)}><i class="fa fa-trash text-danger"></i> Delete</a></li>
-                                <li><a name="actionType" id="edit" onClick={this.selectActiontype.bind(this)}><i class="fa fa-pencil-square-o text-secondary"></i> Edit</a></li>
+                            <button type="button" className ="btn btn-default dropdown-toggle" data-toggle="dropdown">ACTION <span className ="caret"></span></button>
+                            <ul role="menu" className ="dropdown-menu dropdown-menu-right">
+                                <li><a name="actionType" id="activate" onClick={this.selectActiontype.bind(this)}><i className ="fa fa-plus-circle text-primary"></i> Activate</a></li>
+                                <li><a name="actionType" id="deactivate" onClick={this.selectActiontype.bind(this)}><i className ="fa fa-minus-circle text-warning"></i> Deactivate</a></li>
+                                <li><a name="actionType" id="delete" onClick={this.selectActiontype.bind(this)}><i className ="fa fa-trash text-danger"></i> Delete</a></li>
+                                <li><a name="actionType" id="edit" onClick={this.selectActiontype.bind(this)}><i className ="fa fa-pencil-square-o text-secondary"></i> Edit</a></li>
                             </ul>
                         </div>
                         <div className="pull-right searchOcrUser">
-                            <input type="text" className="form-control btn-rounded" placeholder="Search User..."/>
+                            <input type="text" id="searchOcrUser" className="form-control btn-rounded" placeholder="Search User..." onKeyUp={this.handleSearchElement.bind(this)} />
+                            <button className="close-icon"  style={{paddingLeft: "60%",paddingTop: "0px"}}  onClick={this.clearSearchVal.bind(this)}type="reset"></button>
                         </div>
                     </div>
                 </div>
                 <div className = "table-responsive box-shadow">
                         {manageUsersTable}
+                </div>
+                <div className="ma-datatable-footer"  id="idPagination">
+                    <div className="dataTables_paginate">
+                        {paginationTag}
+                    </div>
                 </div>
                 <OcrEditUser/>
             </div>
