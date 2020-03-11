df9fcf93ca204ca7add881cfec9aad4d74343931
sabya93 <sabyasachi.das@marlabs.com>
2020-03-11 13:05:34 +0530

    Loader msg changes, Fixed #1557 and #1566

--- static/react/src/app/components/common/CreateSignalLoader.js
+++ static/react/src/app/components/common/CreateSignalLoader.js
@@ -39,10 +39,10 @@ export class CreateSignalLoader extends React.Component {
             else if((this.props.createSignalLoaderValue >= 0) && (Object.keys(this.props.signalLoadedText).length > 0) && (document.getElementById("loadingMsgs1") != null) && (document.getElementById("loadingMsgs1").innerText === "")){
                 getText = Object.values(this.props.signalLoadedText)
                 this.makeULSig(getText);
-            } 
+            }
     }
-		
- 
+
+
 
 
 
@@ -71,13 +71,13 @@ export class CreateSignalLoader extends React.Component {
                     x.innerHTML = "Step " + i + ": " + array[i];
                     x1.innerHTML ="Step " + (i+1) + ": " + array[i+1];
                     x2.innerHTML ="Step " + (i+2) + ": " + array[i+2];
-                }, 8000 * i);
+                }, 4000 * i);
             })(i);
         }
 				for(var i=array.length-5;i<array.length;i++){
-					x.innerHTML = "Step " + i + ": " + array[i];
-					x1.innerHTML ="Step " + (i+1) + ": " + array[i+1];
-					x2.innerHTML ="Step " + (i+2) + ": " + array[i+2];
+					x.innerHTML = 'Please wait while analysing...';
+					x1.innerHTML ='';
+					x2.innerHTML ='';
 				}
 }
     render() {
@@ -97,13 +97,13 @@ export class CreateSignalLoader extends React.Component {
                 <Modal show={store.getState().signals.createSignalLoaderModal}  backdrop="static" onHide={this.closeModelPopup.bind(this)} dialogClassName="modal-colored-header">
 
                 <Modal.Body style={{marginBottom:"0"}}>
-                
-				
+
+
 				<div className="row">
               <div className="col-md-12">
                 <div className="panel xs-mb-0 modal_bg_processing">
                   <div className="panel-body no-border xs-p-0">
-					
+
 				<div id="text-carousel" class="carousel slide vertical" data-ride="carousel">
 
 				<div class="row">
@@ -112,21 +112,21 @@ export class CreateSignalLoader extends React.Component {
 				<div class="item active">
 				<div class="carousel-content">
 					<h4 className="text-center">
-					mAdvisor - Data scientist in a box 
+					mAdvisor - Data scientist in a box
 				</h4>
 				</div>
 				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
-					One click AutoML solution 
+					One click AutoML solution
 				</h4>
 				</div>
 				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
-					Automated AI and Machine Learning Techniques with zero manual intervention 
+					Automated AI and Machine Learning Techniques with zero manual intervention
 				</h4>
 				</div>
 				</div>
@@ -136,77 +136,77 @@ export class CreateSignalLoader extends React.Component {
 					User friendly interface for Business users with one click solution
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Advanced feature engineering options in analyst mode
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Build predictive models and deploy them for real-time prediction on unseen data
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Suitable for datasets of any size
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Gives you best results from multiple models
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Expandable and scalable adoption of new use cases
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					APD helps users to analyze and create data stories from large volumes of data
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Uses statistical techniques and machine learning algorithms to identify patterns within data sets
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Get insights and conclusive analysis in natural language
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Responsive visualization layer help to create intuitive analysis and bring data to life
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
 					Import dataset from various sources and channels like, Local file system,  MySQL, MSSQL, SAP HANA, HDFS and S3
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
@@ -234,7 +234,7 @@ export class CreateSignalLoader extends React.Component {
 					Narratives for BI - Translates data from charts and visualization into meaningful summaries
 				</h4>
 				</div>
-				</div>				
+				</div>
 				<div class="item">
 				<div class="carousel-content">
 				   <h4 className="text-center">
@@ -256,10 +256,10 @@ export class CreateSignalLoader extends React.Component {
 
 
 				</div>
-					
+
 		<img src={imgsrc_url} className="img-responsive"/>
-                       
-                    
+
+
 					<div className="modal_stepsBlock xs-p-10">
 					<div className="row">
 						<div className="col-sm-9">
@@ -276,39 +276,39 @@ export class CreateSignalLoader extends React.Component {
 									{/* <li class="active">{store.getState().signals.loaderText}</li> */}
 									{/*	<li>----</li>*/}
 								{/* </ul> */}
-							
+
 						</div>
 						<div className="col-sm-3 text-center">
 							{store.getState().signals.createSignalLoaderValue >= 0?<h2 className="text-white">{store.getState().signals.createSignalLoaderValue}%</h2>:<h5 style={{display:"block", textAlign: "center" }} className="loaderValue">In Progress</h5>}
-							
+
 						</div>
 					</div>
 					</div>
-			
-						
+
+
                     {store.getState().signals.createSignalLoaderValue >= 0?<div className="p_bar_body hidden">
                       <progress className="prg_bar" value={store.getState().signals.createSignalLoaderValue} max={95}></progress>
-					  
+
                       {/*<div className="progress-value">
                         <h3>{store.getState().signals.createSignalLoaderValue}
                           %</h3>
                       </div>*/}
-					  
+
                     </div>:""}
                   </div>
                 </div>
               </div>
             </div>
-				
-				
-				
-				
-				
-				
-			 
-			
-			
-			
+
+
+
+
+
+
+
+
+
+
           </Modal.Body>
           {(this.props.showHide)
             ? (
