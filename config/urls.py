5453f5ef7b46cf0976d159352b8f5d58f5b9cc91
marlabsmanoj <manojkumar.nagappa@marlabs.com>
2020-04-03 14:13:06 +0530

    Default url change

--- config/urls.py
+++ config/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     url(r'^api-token-auth/', ObtainJSONWebToken.as_view(serializer_class=myJSONWebTokenSerializer)),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
-    # url(r'^', home_view)
+    url(r'^', home_view)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
