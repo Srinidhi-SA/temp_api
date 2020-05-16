05709eb900770c44233f71b9209e724f1fb5cb6c
rajshreegupta21 <rajshree.gupta@marlabs.com>
2020-05-16 19:45:47 +0530

    ITE UI changes

--- static/react/src/app/components/apps/Ocr/OcrTable.js
+++ static/react/src/app/components/apps/Ocr/OcrTable.js
@@ -290,7 +290,7 @@ export class OcrTable extends React.Component {
     ) : '' : '')
 
     var ShowModel = (<div id="uploadData" role="dialog" className="modal fade modal-colored-header">
-      <Modal show={this.state.showRecognizePopup} onHide={this.closePopup.bind(this)} dialogClassName="modal-colored-header">
+      <Modal backdrop="static" show={this.state.showRecognizePopup} onHide={this.closePopup.bind(this)} dialogClassName="modal-colored-header">
         <Modal.Body style={{ padding: 0 }} >
           <div className="row" style={{ margin: 0 }}>
             <h4 className="text-center">Recognizing Document</h4>
