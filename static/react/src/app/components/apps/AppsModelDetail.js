db7b447a1057645ec943170100b3367a05b4d59f
VidyaR13 <vidya.ramaiah@marlabs.com>
2020-06-11 13:09:01 +0530

    Added new algorithms to create link in summary page and in filter dropdown of model management algorithms column

--- static/react/src/app/components/apps/AppsModelDetail.js
+++ static/react/src/app/components/apps/AppsModelDetail.js
@@ -79,8 +79,16 @@ export class AppsModelDetail extends React.Component {
 					algorithmName = "PT"
 				else if(algoNam === "NEURALNETWORK(TENSORFLOW)")
 				  algorithmName = "TF"
-				else algorithmName = "NN"
+				else if(algoNam === "NEURALNETWORK(SKLEARN)")
+				  algorithmName = "NN"
+				else if(algoNam === "ENSEMBLE")
+				  algorithmName = "EN"
+				else if(algoNam === "ADABOOST")
+				  algorithmName = "ADAB"
+				else 
+				  algorithmName = ""
 				
+				if(algorithmName != ""){
 					let info = document.createElement('a');
 					var att = document.createAttribute("class");
 					att.value = "summaryLink";
@@ -92,6 +100,7 @@ export class AppsModelDetail extends React.Component {
 					info.href = (sel.length !=0)?
 					this.props.match.url.replace("models/"+this.props.modelSlug,"modelManagement/"+sel[0].slug):"#"
 					$(".sm-mb-20")[i].parentNode.parentNode.appendChild(info);
+				}
 			}
 
 	}
