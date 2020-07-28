812e15a548f035e477a3330c82632520687c7670
sabya93 <sabyasachi.das@marlabs.com>
2020-07-28 16:32:23 +0530

    added wait

--- ocr/ITE/scripts/apis.py
+++ ocr/ITE/scripts/apis.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 import requests
 from google.cloud import vision
 import io
@@ -13,23 +15,38 @@ class Api_Call:
 
     def text_from_Azure_API(self):
 
+        # For Azure OCR API.
+
         subscription_key = "8f6ad67b6c4344779e6148ddc48d96c0"
         vision_base_url = "https://madvisor.cognitiveservices.azure.com/vision/v3.0/"
-
+        #        vision_base_url = \
+        #            "https://westcentralus.api.cognitive.microsoft.com/vision/v3.0/"
         text_recognition_url = vision_base_url + "read/analyze"
+
         data = open(self.doc_path, "rb").read()
 
         headers = {'Ocp-Apim-Subscription-Key': subscription_key,
                    'Content-Type': 'application/octet-stream'}
 
-        response = requests.post(
-            text_recognition_url,
-            headers=headers,
-            data=data)
-        response.raise_for_status()
-
         analysis = {}
         poll = True
+        wait = 0
+
+        while wait < 3:
+            try:
+                response = requests.post(
+                    text_recognition_url,
+                    headers=headers,
+                    data=data)
+                response.raise_for_status()
+                wait = 4
+            except:
+                wait = wait + 1
+                wait_time = int(np.random.uniform(1, 4))
+                print('Waiting')
+                time.sleep(wait_time)
+                if wait >= 3: raise
+
         while (poll):
             response_final = requests.get(
                 response.headers["Operation-Location"], headers=headers)
@@ -39,9 +56,6 @@ class Api_Call:
                 poll = False
 
         self.doc_analysis = analysis
-
-    #        time.sleep(5)
-
     def page_wise_response(self, page_number):
         return [i for i in self.doc_analysis["analyzeResult"]["readResults"]
                 if (i["page"] == page_number)][0]
