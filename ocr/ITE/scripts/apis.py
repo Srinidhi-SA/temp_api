8d98f6a3caca30d861f3f8ea652d09267cd037e5
sabya93 <sabyasachi.das@marlabs.com>
2020-08-06 11:55:19 +0530

    added API key param

--- ocr/ITE/scripts/apis.py
+++ ocr/ITE/scripts/apis.py
@@ -22,11 +22,13 @@ class Api_Call:
         print('#' * 50, '\n', datetime.datetime.now(), '\nAPI called\n', '#' * 50)
         subscription_key = "8f6ad67b6c4344779e6148ddc48d96c0"
         api_gateway_url = 'https://74psiiujd1.execute-api.us-east-2.amazonaws.com/dev'
+        api_key = 'cxKOAtuMXwazGWTLOI0Ks21dttO2Y9CX68XeHnoJ'
 
         data = open(self.doc_path, "rb").read()
 
         headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-                   'Content-Type': 'application/octet-stream'}
+                   'Content-Type': 'application/octet-stream',
+                   'x-api-key': api_key}
 
         analysis = {}
         poll = True
