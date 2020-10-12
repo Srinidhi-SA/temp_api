da8f25c456b90c70f5e1243dc81bd8cb21ddf37f
sabya93 <sabya93@gmail.com>
2020-10-12 14:31:43 +0530

    image enhancement changes

--- ocr/views.py
+++ ocr/views.py
@@ -1339,7 +1339,9 @@ class OCRImageView(viewsets.ModelViewSet, viewsets.GenericViewSet):
                 response = HttpResponse(result, content_type="application/xml")
                 response['Content-Disposition'] = 'attachment; filename={}.xml'.format(data['slug'])
             elif data['format'] == 'csv':
-                result = json_2_csv(json.loads(result))
+                df = timesheet_main(final_result)
+                result = df.to_csv()
+                # result = json_2_csv(json.loads(result))
                 response = HttpResponse(result, content_type="application/text")
                 response['Content-Disposition'] = 'attachment; filename={}.csv'.format(data['slug'])
             else:
