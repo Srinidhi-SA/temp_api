e6121f43c42b197573f4e834f500ff1981db7091
Srinidhi-SA <56720474+Srinidhi-SA@users.noreply.github.com>
2020-12-01 20:49:12 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ docker build -t nginx .
 docker tag nginx 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest
 $(aws ecr get-login --region us-east-1 --no-include-email) 
 docker push 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest 
+cd ..
