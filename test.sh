cf82286da0a2bb4b516eb9eff55e5b5f9d989a5e
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2020-12-01 19:53:11 +0530

    buildspec.yml

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-
+cd nginx_docker
 sudo  docker build -t nginx .
 sudo docker tag nginx 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest
 sudo $(aws ecr get-login --region us-east-1 --no-include-email) 
