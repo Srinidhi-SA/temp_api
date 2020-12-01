6555a393e1a0ddd650c405ad4740fde3aedd39ae
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2020-12-01 20:36:51 +0530

    Code changes in script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 cd nginx_docker
-sudo  docker build -t nginx .
-sudo docker tag nginx 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest
-sudo $(aws ecr get-login --region us-east-1 --no-include-email) 
-sudo docker push 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest 
+docker build -t nginx .
+docker tag nginx 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest
+$(aws ecr get-login --region us-east-1 --no-include-email) 
+docker push 115502491259.dkr.ecr.us-east-1.amazonaws.com/madvisor_nginx:latest 
