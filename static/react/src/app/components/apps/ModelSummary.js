8e911e732295ec596638d0b7de2543b9354aa2d2
VidyaR13 <vidya.ramaiah@marlabs.com>
2021-03-30 11:57:26 +0530

    MMSummary code optimised

--- static/react/src/app/components/apps/ModelSummary.js
+++ static/react/src/app/components/apps/ModelSummary.js
@@ -4,15 +4,10 @@ import {getDeploymentList} from "../../actions/appActions";
 import {STATIC_URL} from "../../helpers/env.js"
 import {openDeployModalAction, closeDeployModalAction} from "../../actions/modelManagementActions"
 import {isEmpty,getUserDetailsOrRestart} from "../../helpers/helper";
-import {getAlgoAnalysis} from "../../actions/signalActions";
-import {CardHtml} from "../../components/signals/CardHtml";
-import {CardTable} from "../common/CardTable";
-import {DataBox} from "../common/DataBox";
-import $ from "jquery";
+import {getAlgoAnalysis, setmmLoaderFlag} from "../../actions/signalActions";
 import { Deployment } from "./Deployment";
-import { ModelSummeryButton } from "../common/ModelSummeryButton";
-import { C3ChartNew } from "../C3ChartNew";
-import { Scrollbars } from "react-custom-scrollbars";
+import { Card } from "../signals/Card";
+import Scrollbars from "react-custom-scrollbars/lib/Scrollbars";
 
 @connect((store) => {
   return {
@@ -29,6 +24,7 @@ export class ModelSummary extends React.Component {
   componentWillMount() {
   this.props.dispatch(getAlgoAnalysis(getUserDetailsOrRestart.get().userToken, this.props.match.params.slug));
   this.props.dispatch(getDeploymentList(this.props.match.params.slug));
+  this.props.dispatch(setmmLoaderFlag(true));
   }
 
   componentDidMount(){
@@ -43,81 +39,40 @@ export class ModelSummary extends React.Component {
   window.history.back();
   }
   
-  calculateWidth(width){
-  let colWidth  = parseInt((width/100)*12);
-  let divClass="col-md-"+colWidth;
-  return divClass;
-  }
-  
-  renderCardData(c3,cardWidth){
-  var htmlData = c3.map((story, i) => {
-    let randomNum = Math.random().toString(36).substr(2,8);
-    switch (story.dataType) {
-      case "html":
-            if(!story.hasOwnProperty("classTag"))story.classTag ="none";
-            return (<CardHtml key={randomNum} htmlElement={story.data} type={story.dataType} classTag={story.classTag}/>);
-            break;
-      case "c3Chart":
-            let chartInfo=[]
-            if(!$.isEmptyObject(story.data)){
-            if(story.chartInfo){
-              chartInfo=story.chartInfo
-            }
-            if(story.widthPercent == 100){
-              let divClass="";
-              let parentDivClass = "col-md-12";
-              if(!cardWidth || cardWidth > 50)
-              divClass = "col-md-12"
-              else
-              divClass = "col-md-12";
-              let sideChart=false;
-              return (
-                <div key={randomNum} className={parentDivClass} style={{height:"350px"}} >
-                {this.state.loader && <div className="mmLoader">
-                  <img src={STATIC_URL+"assets/images/loaderChart.gif"} style={{margin:"auto"}}></img>
-                </div>}
-                <div className={divClass} style={{display:(this.state.loader?"none":"inline-block"),paddingLeft:"30px",position:"relative",height:"350px"}}>
-                  <C3ChartNew chartInfo={chartInfo} sideChart={sideChart} classId={randomNum}  widthPercent = {story.widthPercent} data={story.data.chart_c3}  yformat={story.data.yformat} y2format={story.data.y2format} guage={story.data.gauge_format} tooltip={story.data.tooltip_c3} tabledata={story.data.table_c3} tabledownload={story.data.download_url} xdata={story.data.xdata}/>
-                </div>
-              </div>);
-            }
-            }
-            break;
-      case "table":
-            if(!story.tableWidth)story.tableWidth = 100;
-            var colClass= this.calculateWidth(story.tableWidth)
-            colClass = colClass;
-            return (<div className={colClass} key={randomNum}><CardTable  jsonData={story.data} type={story.dataType}/></div>);
-            break;
-      case "dataBox":
-            if(this.props.algoAnalysis.app_id == 2){
-              return (<DataBox  key={i} jsonData={story.data} type={story.dataType}/>);
-            }else{
-              return( 
-                story.data.map((data)=>{
-                return (
-                  <div className="col-md-5ths col-sm-6 col-xs-12 bgStockBox">
-                    <h3 className="text-center">{data.value}<br/><small>{data.name}</small></h3>
-                    <Scrollbars className="performanceCard" style={{height:"78px"}} renderTrackHorizontal={props => <div {...props} style={{display: 'none'}} className="track-horizontal"/>}>
-                      <div style={{padding:"5px 10px"}} >{data.description}</div>
-                    </Scrollbars>
-                  </div>
-                );
-              })
-              );
-            }
-            break;
-      case "button":
-            return (<ModelSummeryButton key={randomNum} data={story.data.chart_c3} tabledownload={story.data.download_url} classId={randomNum} type={story.dataType}/>);
-            break;
-    }
-    });
-  return htmlData;
+  markFlag(){
+    this.props.dispatch(setmmLoaderFlag(false));
   }
 
-  markFlag(){
-    this.setState({loader:false})
+  getCards(i){
+    let that = this;
+    let cardDataList = "";
+    let listOfCardList = this.props.algoAnalysis.data.listOfNodes[i].listOfCards;
+    cardDataList = listOfCardList.map((data, i) => {
+      let appId = that.props.algoAnalysis.app_id
+      var cardDataArray = data.cardData;
+      if(cardDataArray.length>0){
+        if(cardDataArray[0].dataType === "dataBox"){
+          if(cardDataArray[0].data.length === 5){
+            let columnsTemplates = cardDataArray[0].data.map((data,i)=>{
+              return(<div key={i} className="col-md-5ths col-sm-6 col-xs-12 bgStockBox">
+                <h3 className="text-center">{data.value}<br/><small>{data.name}</small></h3>
+                  <Scrollbars className="performanceCard" style={{height:"78px"}} renderTrackHorizontal={props => <div {...props} style={{display: 'none'}} className="track-horizontal"/>}>
+                    <div style={{padding:"5px 10px"}} >{data.description}</div>
+                  </Scrollbars>
+                </div>);
+              });
+              return <div className="row ov_card_boxes">{columnsTemplates}</div>;
+            }else{
+              return (<div key={i} className="row ov_card_boxes"><Card cardData={cardDataArray} cardWidth={100}/></div>)
+            }
+        }else{
+          return (<div key={i} className= {data.cardWidth===100?"col-md-12":"col-md-6"}><Card cardData={cardDataArray} cardWidth={data.cardWidth}/></div>)
+        }
+      }
+    });
+    return cardDataList
   }
+
 
     render(){
     if(isEmpty(this.props.algoAnalysis)){
@@ -135,141 +90,9 @@ export class ModelSummary extends React.Component {
       </div>
     );
     }else{
-      var overviewCard = "";
-      var performanceCard="";
       var algoAnalysis = this.props.algoAnalysis;
-      
-      var overviewPage = this.props.algoAnalysis.data.listOfNodes.filter(row => row.name === "Overview");
-      var oVtop = overviewPage.map(card => card.listOfCards);
-      let olen = oVtop[0].length;
-      var j =0;
-      var loop = 0;
-
-      var th = [];    //table heading
-      var tdata = [];   //table data
-      var tw = [];    //table width
-      var tableHeading = [];
-      var tableData = [];
-
-      //Rendering Summary and Settings Tables
-      for(j=0;j<olen;j++){
-        loop ++;
-        if(loop-1 == j){
-          th[j] = oVtop.map(fun => fun[j].cardData[0])
-          tdata[j] = oVtop.map(fun => fun[j].cardData[1])
-          tw[j] = oVtop.map(fun => fun[j].cardWidth)[0]
-        }
-        if(j == 0){
-          tableHeading[0] = this.renderCardData(th[j],tw[j]);
-          tableData[0] = this.renderCardData(tdata[j],tw[j]);
-        }else{
-          tableHeading[1] = this.renderCardData(th[j],tw[j]);
-          tableData[1] = this.renderCardData(tdata[j],tw[j]);
-        }
-      }
-
-      var performancePage = this.props.algoAnalysis.data.listOfNodes.filter(row => row.name === "Performance");
-      var top = performancePage.map(card => card.listOfCards);
-      let plen = top[0].length;
-      var i = 0;
-      var count = 0;
-
-      var chartData = []
-      var chartHeading = []
-      var chartButton = []
-      var h = []      //card heading
-      var cd = []     //card data
-      var w = []      //card width
-      var topCards = ""
-      var button = []
-
-      //Rendering Charts
-      for(i=0;i<plen;i++){
-        if(i == 0){
-          //render top cards for first iteration
-          cd[i] = top.map(fun => fun[i].cardData[0])
-          w[i] = top.map(fun => fun[i].cardWidth)[0]
-          topCards = this.renderCardData(cd[i],w[i]);
-        }else{
-          count ++;
-          //pick chart values
-          if(count == i){
-            h[i] = top.map(fun => fun[i].cardData[0])
-            cd[i] = top.map(fun => fun[i].cardData[1])
-            w[i] = top.map(fun => fun[i].cardWidth)[0]
-            if(this.props.algoAnalysis.app_id == 13 && count == 1){
-              button[0] = top.map(fun => fun[i].cardData[2])
-            }
-          }
-          //Render chart values
-          if(count == 1){
-            chartHeading[1] = this.renderCardData(h[1],w[1]);
-            chartData[1] = this.renderCardData(cd[1],w[1]);
-            if(this.props.algoAnalysis.app_id == 13 && count == 1){
-              chartButton[1] = this.renderCardData(button[0],w[1]);
-            }
-          }else if(count == 2){
-            chartHeading[2] = this.renderCardData(h[2],w[2]);
-            chartData[2] = this.renderCardData(cd[2],w[2]);
-          }else if(count == 3){
-            chartHeading[3] = this.renderCardData(h[3],w[3]);
-            chartData[3] = this.renderCardData(cd[3],w[3]);
-          }else if(this.props.algoAnalysis.app_id == 2 && count == 4){
-            chartHeading[4] = this.renderCardData(h[4],w[4]);
-            chartData[4] = this.renderCardData(cd[4],w[4]);
-          }
-        }
-      }
-      
-      overviewCard=(
-        <div class="row">
-          <div class="col-md-6">
-            <div className="sm-ml-15 sm-pb-10">{tableHeading[0]}</div>
-            {tableData[0]}
-          </div>
-          <div class="col-md-6">
-            <div className="sm-ml-15 sm-pb-10">{tableHeading[1]}</div>
-            {tableData[1]}
-          </div>
-        </div>
-      )
-      
-      let newClass = ""
-      if(this.props.algoAnalysis.app_id == 13){
-        newClass = "col-md-12";
-      }else{
-        newClass = "col-md-6"
-      }
-      performanceCard = (
-        <div>
-          <div class="row ov_card_boxes">
-            {topCards} 
-          </div>
-          <div class="row xs-mt-10">
-            <div class="col-md-6">
-              {chartHeading[1]}
-              {chartData[1]}
-              {chartButton[1]}
-            </div>
-            <div class="col-md-6">
-              {chartHeading[2]}
-              {chartData[2]}
-            </div>
-          </div>
-          <hr/>
-          <div class="row xs-mt-10">
-            <div class= {newClass} >  
-              {chartHeading[3]}
-              {chartData[3]}
-            </div>
-            <div class={newClass}>
-              {chartHeading[4]}
-              {chartData[4]}
-            </div>
-          </div>
-        </div>
-      )
-    }
+      let overview = this.getCards(0);
+      let performance = this.getCards(1);
 
     return (
       <div class="side-body">
@@ -286,17 +109,13 @@ export class ModelSummary extends React.Component {
                   <li><a href="#deployment" data-toggle="tab">Deployment</a></li>
                 </ul>
                 <div class="tab-content xs-pt-20">
-                  <div id="overview" class="tab-pane active cont">                
-                    {overviewCard}
-                  </div>
-                  <div id="performance" class="tab-pane cont">
-                    {performanceCard}
-                  </div>
+                  <div id="overview" class="tab-pane active cont">{overview}</div>
+                  <div id="performance" class="tab-pane cont">{performance}</div>
                   <div id="deployment" class="tab-pane cont">
                     <Deployment/>
                   </div>
                 </div>
-                <div class="buttonRow text-right"> <a href="javascript:;" onClick={this.closeModelSummary.bind(this)}class="btn btn-primary">Close </a> </div>
+                <div class="buttonRow text-right clearfix"> <a href="javascript:;" onClick={this.closeModelSummary.bind(this)}class="btn btn-primary">Close </a> </div>
               </div>
             </div>
           </div>
@@ -304,6 +123,7 @@ export class ModelSummary extends React.Component {
       </div>
     );
   }
+}
   openDeployModal(item) {
     this.props.dispatch(openDeployModalAction(item));
   }
